964204e5051f56b365d7d9ec88f52d7297edf590
jis <jis@netbeans.org>
2007-11-02 15:41:15 +0000

    #120956 - fixed incorrect value conversion

--- lib.profiler.ui/src/org/netbeans/lib/profiler/ui/memory/DiffLivenessResultsPanel.java
+++ lib.profiler.ui/src/org/netbeans/lib/profiler/ui/memory/DiffLivenessResultsPanel.java
@@ -125,7 +125,8 @@ public class DiffLivenessResultsPanel extends SnapshotLivenessResultsPanel {
             case 4:
                 return ((nTrackedAllocObjects[index] > 0) ? "+" : "") + intFormat.format(nTrackedAllocObjects[index]); // NOI18N
             case 5:
-                return ((avgObjectAge[index] > 0) ? "+" : "") + StringUtils.floatPerCentToString(avgObjectAge[index]); // NOI18N
+                // NOTE: StringUtils.floatPerCentToString() doesn't handle correctly negative values!
+                return ((avgObjectAge[index] > 0) ? "+" : "-") + StringUtils.floatPerCentToString(Math.abs(avgObjectAge[index])); // NOI18N
             case 6:
                 return ((maxSurvGen[index] > 0) ? "+" : "") + intFormat.format(maxSurvGen[index]); // NOI18N
             case 7:
