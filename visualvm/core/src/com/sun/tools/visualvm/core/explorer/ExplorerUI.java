d2cb03fa3eda63902950f20182b7ffa3f19a1b6c
Jiri Sedlacek <jsedlacek@java.net>
2008-02-20 18:12:32 +0000

    Fixed line ends
    git-svn-id: https://svn.java.net/svn/visualvm~svn/trunk@200 ba0df5e5-9053-6d9f-941f-f112716aa91a

--- visualvm/core/src/com/sun/tools/visualvm/core/explorer/ExplorerUI.java
+++ visualvm/core/src/com/sun/tools/visualvm/core/explorer/ExplorerUI.java
@@ -1,49 +1,26 @@
 /*
-
  * Copyright 2007-2008 Sun Microsystems, Inc.  All Rights Reserved.
-
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
-
  *
-
  * This code is free software; you can redistribute it and/or modify it
-
  * under the terms of the GNU General Public License version 2 only, as
-
  * published by the Free Software Foundation.  Sun designates this
-
  * particular file as subject to the "Classpath" exception as provided
-
  * by Sun in the LICENSE file that accompanied this code.
-
  *
-
  * This code is distributed in the hope that it will be useful, but WITHOUT
-
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-
  * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-
  * version 2 for more details (a copy is included in the LICENSE file that
-
  * accompanied this code).
-
  *
-
  * You should have received a copy of the GNU General Public License version
-
  * 2 along with this work; if not, write to the Free Software Foundation,
-
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
-
  *
-
  * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa Clara,
-
  * CA 95054 USA or visit www.sun.com if you need additional information or
-
  * have any questions.
-
  */
 
 
@@ -67,7 +44,6 @@ import javax.swing.JScrollPane;
 import javax.swing.JTree;
 import javax.swing.KeyStroke;
 import javax.swing.ScrollPaneConstants;
-import javax.swing.UIManager;
 import javax.swing.event.TreeExpansionEvent;
 import javax.swing.event.TreeWillExpandListener;
 import javax.swing.tree.ExpandVetoException;
