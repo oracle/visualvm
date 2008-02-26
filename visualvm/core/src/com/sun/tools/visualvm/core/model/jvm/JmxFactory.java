6a867f89f51b2231e3081547ee0cc7f668602525
Luis Miguel Alventosa <lmalvent@java.net>
2008-02-26 22:22:15 +0000

    Fix javadoc for JmxFactory.
    git-svn-id: https://svn.java.net/svn/visualvm~svn/trunk@247 ba0df5e5-9053-6d9f-941f-f112716aa91a

--- visualvm/core/src/com/sun/tools/visualvm/core/model/jvm/JmxFactory.java
+++ visualvm/core/src/com/sun/tools/visualvm/core/model/jvm/JmxFactory.java
@@ -1,8 +1,4 @@
 /*
- * Model.java
- *
- * Created on December 18, 2007, 6:13 PM
- *
  * Copyright 2007-2008 Sun Microsystems, Inc.  All Rights Reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
