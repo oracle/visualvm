2355b85d315c14897cb0e9c8b34e3473434d3a6f
Tomas Hurka <thurka@java.net>
2008-03-06 11:47:24 +0000

    options cleanup
    git-svn-id: https://svn.java.net/svn/visualvm~svn/trunk@418 ba0df5e5-9053-6d9f-941f-f112716aa91a

--- plugins/jconsole/src/com/sun/tools/visualvm/modules/jconsole/options/JConsoleSettings.java
+++ plugins/jconsole/src/com/sun/tools/visualvm/modules/jconsole/options/JConsoleSettings.java
@@ -32,129 +32,62 @@ import javax.swing.event.ChangeListener;
 import org.openide.util.NbPreferences;
 
 
-public class JConsoleSettings implements ChangeListener
-{
+public class JConsoleSettings implements ChangeListener {
     static final long serialVersionUID = -1055706114162507505L;
-
+    
     private static final String PROP_POLLING     = "POLLING"; // NOI18N
     private static final String PROP_TILE   = "TILE"; // NOI18N
     private static final String PROP_CLASSPATH  = "CLASSPATH"; // NOI18N
     private static final String PROP_PLUGINSPATH  = "PLUGINS"; // NOI18N
-    private static final String PROP_VM_OPTIONS = "VM_OPTIONS"; //NOI18N
-    private static final String PROP_OTHER_ARGS = "OTHER_ARGS"; //NOI18N
-    private static final String PROP_URL = "DEFAULT_URL"; //NOI18N
     
-    public static String NETBEANS_CLASS_PATH;
     private Preferences pref;
     
     private static Boolean greater;
     private static JConsoleSettings INSTANCE;
     
-    JConsoleSettings() 
-    {
+    JConsoleSettings() {
         pref = NbPreferences.forModule(JConsoleSettings.class);
-        setPolling(4);
-        setTile(Boolean.TRUE);
     }
     
-    public static boolean isNetBeansJVMGreaterThanJDK15() {
-        if(greater == null) {
-            //Check if we are running on 1.6 minimum
-            try {
-                Class.forName("javax.swing.SwingWorker");// NOI18N
-               greater = true;
-            }catch(ClassNotFoundException e) {
-                greater = false;
-            }
-        }
-        return greater;
-    }
-    
-    public String displayName ()
-    {
-        return "JConsole settings";// NOI18N
-    }    
-
-    public static synchronized JConsoleSettings getDefault () 
-    {
+    public static synchronized JConsoleSettings getDefault() {
         if (INSTANCE == null) {
             INSTANCE = new JConsoleSettings();
         }
         return INSTANCE;
     }
     
-    public void stateChanged (ChangeEvent e)
-    {
+    public void stateChanged(ChangeEvent e) {
     }
     
-    public Boolean getTile()
-    {
+    public Boolean getTile() {
         return pref.getBoolean(PROP_TILE,Boolean.TRUE);
     }
-
-    public void setTile(Boolean tile)
-    {
+    
+    public void setTile(Boolean tile) {
         pref.putBoolean(PROP_TILE, tile);
     }
     
-    public String getClassPath()
-    {
+    public String getClassPath() {
         return pref.get(PROP_CLASSPATH,null);
     }
-
-    public void setClassPath(String value)
-    {
+    
+    public void setClassPath(String value) {
         pref.put(PROP_CLASSPATH, value);
     }
     
-    public String getPluginsPath()
-    {
+    public String getPluginsPath() {
         return pref.get(PROP_PLUGINSPATH,null);
     }
-
-    public void setPluginsPath(String value)
-    {
+    
+    public void setPluginsPath(String value) {
         pref.put(PROP_PLUGINSPATH, value);
     }
     
-    public Integer getPolling()
-    {
+    public Integer getPolling() {
         return pref.getInt(PROP_POLLING,4);
     }
     
-    public void setPolling(Integer polling)
-    {   
+    public void setPolling(Integer polling) {
         pref.putInt(PROP_POLLING, polling);
     }
-    
-    public String getVMOptions()
-    {
-        return pref.get(PROP_VM_OPTIONS,null);
-    }
-    
-    public void setVMOptions(String other)
-    {
-        pref.put(PROP_VM_OPTIONS, other);
-    }
-    
-    public String getOtherArgs()
-    {
-        return pref.get(PROP_OTHER_ARGS,null);
-    }
-    
-    public void setOtherArgs(String other)
-    {
-        pref.put(PROP_OTHER_ARGS, other);
-    }
-    
-    public String getDefaultUrl()
-    {
-        return pref.get(PROP_URL,null);
-    }
-    
-    public void setDefaultUrl(String url)
-    {
-        pref.put(PROP_URL, url);
-    }
-    
 }
