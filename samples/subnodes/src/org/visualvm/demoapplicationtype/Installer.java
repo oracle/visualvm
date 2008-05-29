c2224028c53848872dba2cdccb4e657924830ed0
Geertjan Wielenga <geertjan@java.net>
2008-05-29 10:36:58 +0000

    cleaning up the sample -- removing files that were not needed
    git-svn-id: https://svn.java.net/svn/visualvm~svn/trunk@1086 ba0df5e5-9053-6d9f-941f-f112716aa91a

--- samples/subnodes/src/org/visualvm/demoapplicationtype/Installer.java
+++ samples/subnodes/src/org/visualvm/demoapplicationtype/Installer.java
@@ -5,14 +5,12 @@ import org.visualvm.demoapplicationtype.application.AnagramApplicationProvider;
 import org.openide.modules.ModuleInstall;
 import org.visualvm.demoapplicationtype.datasource.AnagramDataSourceDescriptorProvider;
 import org.visualvm.demoapplicationtype.model.AnagramModelProvider;
-import org.visualvm.demoapplicationtype.overview.AnagramOverviewProvider;
 
 public class Installer extends ModuleInstall {
 
     @Override
     public void restored() {
         AnagramApplicationTypeFactory.initialize();
-        AnagramOverviewProvider.initialize();
         AnagramDataSourceDescriptorProvider.initialize();
         AnagramModelProvider.initialize();
         AnagramApplicationProvider.initialize();
@@ -21,7 +19,6 @@ public class Installer extends ModuleInstall {
     @Override
     public void uninstalled() {
         AnagramApplicationTypeFactory.shutdown();
-        AnagramOverviewProvider.shutdown();
         AnagramDataSourceDescriptorProvider.shutdown();
         AnagramModelProvider.shutdown();
         AnagramApplicationProvider.shutdown();
