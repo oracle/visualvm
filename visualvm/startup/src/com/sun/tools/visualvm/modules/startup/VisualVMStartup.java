bfd64773ccd0817e7c155cb35ead5bd02e42df86
Jiri Sedlacek <jsedlacek@java.net>
2008-08-18 11:01:54 +0000

    Improved startup dialog for unsupported Java version, added I18N
    git-svn-id: https://svn.java.net/svn/visualvm~svn/trunk@1474 ba0df5e5-9053-6d9f-941f-f112716aa91a

--- visualvm/startup/src/com/sun/tools/visualvm/modules/startup/VisualVMStartup.java
+++ visualvm/startup/src/com/sun/tools/visualvm/modules/startup/VisualVMStartup.java
@@ -24,61 +24,90 @@
  */
 package com.sun.tools.visualvm.modules.startup;
 
+import java.text.MessageFormat;
 import java.util.Properties;
-import java.util.logging.Logger;
+import java.util.ResourceBundle;
 import javax.swing.JOptionPane;
 import javax.swing.UIManager;
 import org.openide.LifecycleManager;
 import org.openide.modules.ModuleInstall;
+import org.openide.util.NbBundle;
 
 /**
  * Manages a module's lifecycle. Remember that an installer is optional and
  * often not needed at all.
  */
 public class VisualVMStartup extends ModuleInstall {
-    private static final Logger LOGGER = Logger.getLogger(VisualVMStartup.class.getName());
+    
+    private static final boolean DISABLE_STARTUP_CHECK = Boolean.getBoolean("com.sun.tools.visualvm.modules.startup.DisableStartupCheck"); // NOI18N
+    
+    private static final ResourceBundle bundle = NbBundle.getBundle(VisualVMStartup.class);
+    private static final String ERROR_STARTUP_CAPTION = bundle.getString("VisualVMStartup_ErrorStartupCaption"); // NOI18N
+    private static final String INCORRECT_VERSION_MSG = bundle.getString("VisualVMStartup_IncorrectVersionMsg"); // NOI18N
+    private static final String JRE_MSG = bundle.getString("VisualVMStartup_JreMsg"); // NOI18N
+    
     
     public void validate() {
-        if (!isCurrentJavaSupported()) {
-            try {
-                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-            } catch (Exception e) {}
-
-            JOptionPane.showMessageDialog(null, "You are running VisualVM using an unsupported  \n"+
-                                                "version of Java (" + System.getProperty("java.version", "unknown version") + "),\n"+
-                                                "from (" + System.getProperty("java.home", "unknown version") + "), startup terminated.  \n\n"+
-                                                "Please refer to https://visualvm.dev.java.net  \n"+
-                                                "for information about requirements and setup.  \n\n",
-                                                "Error Starting VisualVM", JOptionPane.ERROR_MESSAGE);
-            LifecycleManager.getDefault().exit();
+        
+        if (DISABLE_STARTUP_CHECK) {
+            System.err.println("Starting with com.sun.tools.visualvm.modules.startup.DisableStartupCheck=true"); // NOI18N
+        } else {
+            if (!isJava6or7()) {
+                displayError6or7();
+                LifecycleManager.getDefault().exit();
+            } else if (!isJDK()) {
+                displayErrorJRE();
+                LifecycleManager.getDefault().exit();
+            }
         }
     }
     
-    private static boolean isCurrentJavaSupported() {
-        
-        String javaVersion = System.getProperty("java.version");
-
-        Class desktopClass = null;
-        try { desktopClass = Class.forName("java.awt.Desktop"); } catch (ClassNotFoundException ex) {}
-
+    private static void setSystemLaF() {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {}
+    }
+    
+    private static void displayError6or7() {
+        setSystemLaF();
+        JOptionPane.showMessageDialog(null, MessageFormat.format(INCORRECT_VERSION_MSG,
+                new Object[] { System.getProperty("java.specification.version"), getJavaInfo(), // NOI18N
+                getJvmInfo(), System.getProperties().getProperty("java.home", "unknown location") }), // NOI18N
+                ERROR_STARTUP_CAPTION, JOptionPane.ERROR_MESSAGE);
+    }
+    
+    private static void displayErrorJRE() {
+        setSystemLaF();
+        JOptionPane.showMessageDialog(null, MessageFormat.format(JRE_MSG, new Object[] { getJavaInfo(),
+                getJvmInfo(), System.getProperties().getProperty("java.home", "unknown location") }), // NOI18N
+                ERROR_STARTUP_CAPTION, JOptionPane.ERROR_MESSAGE);
+    }
+    
+    private static boolean isJava6or7() {
+        String javaVersion = System.getProperty("java.specification.version"); // NOI18N
+        if (javaVersion == null) return false;
+        return javaVersion.startsWith("1.6") || javaVersion.startsWith("1.7"); // NOI18N
+    }
+    
+    private static boolean isJDK() {
         Class vmClass = null;
-        try { vmClass = Class.forName("com.sun.tools.attach.VirtualMachine"); } catch (ClassNotFoundException ex) {}
-
-        boolean isCurrentJavaSupported = (javaVersion.startsWith("1.6") || javaVersion.startsWith("1.7")) && desktopClass != null && vmClass != null;
-        if (!isCurrentJavaSupported) {
-            LOGGER.warning("Error starting VisualVM, unsupported Java version: " + getJavaInfo());
-        }
-        return isCurrentJavaSupported;
+        try { vmClass = Class.forName("com.sun.tools.attach.VirtualMachine"); } catch (ClassNotFoundException ex) {} // NOI18N
+        return vmClass != null;
     }
     
     private static String getJavaInfo() {
         Properties systemProperties = System.getProperties();
-        String javaVersion = systemProperties.getProperty("java.version", "unknown version");
-        String vmName = systemProperties.getProperty("java.vm.name", "unknown name");
-        String vmVerison = systemProperties.getProperty("java.vm.version", "");
-        String vmInfo = systemProperties.getProperty("java.vm.info", "");
-        String vendorInfo = systemProperties.getProperty("java.vendor", "unknown vendor");
-        return javaVersion + "; " + vmName + " (" + vmVerison + ", " + vmInfo + "); " + vendorInfo;
+        String javaVersion = systemProperties.getProperty("java.version", "unknown version"); // NOI18N
+        String vendorInfo = systemProperties.getProperty("java.vendor", "unknown vendor"); // NOI18N
+        return javaVersion + "; " + vendorInfo; // NOI18N
+    }
+    
+    private static String getJvmInfo() {
+        Properties systemProperties = System.getProperties();
+        String vmName = systemProperties.getProperty("java.vm.name", "unknown name"); // NOI18N
+        String vmVerison = systemProperties.getProperty("java.vm.version", ""); // NOI18N
+        String vmInfo = systemProperties.getProperty("java.vm.info", ""); // NOI18N
+        return vmName + " (" + vmVerison + ", " + vmInfo + ")"; // NOI18N
     }
     
 }
