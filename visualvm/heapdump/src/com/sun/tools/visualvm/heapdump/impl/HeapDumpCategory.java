6d6e06924ca1e674a20908ba2490aa843d4c6124
Tomas Hurka <thurka@java.net>
2008-10-01 12:05:20 +0000

    #195 - use file content to detect heap dump
    git-svn-id: https://svn.java.net/svn/visualvm~svn/trunk@1560 ba0df5e5-9053-6d9f-941f-f112716aa91a

--- visualvm/heapdump/src/com/sun/tools/visualvm/heapdump/impl/HeapDumpCategory.java
+++ visualvm/heapdump/src/com/sun/tools/visualvm/heapdump/impl/HeapDumpCategory.java
@@ -29,14 +29,20 @@ import com.sun.tools.visualvm.core.snapshot.SnapshotCategory;
 import com.sun.tools.visualvm.core.ui.DataSourceWindowManager;
 import com.sun.tools.visualvm.heapdump.HeapDump;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import javax.swing.filechooser.FileFilter;
 import org.openide.util.NbBundle;
 
 /**
  *
  * @author Jiri Sedlacek
+ * @author Tomas Hurka
  */
 public class HeapDumpCategory extends SnapshotCategory<HeapDump> {
-    
+    private static final String HPROF_HEADER = "JAVA PROFILE 1.0";
+    private static final long MIN_HPROF_SIZE = 1024*1024L;
     private static final String NAME = NbBundle.getMessage(HeapDumpCategory.class, "LBL_Heap_Dumps");   // NOI18N
     private static final String PREFIX = "heapdump";    // NOI18N
     private static final String SUFFIX = ".hprof";  // NOI18N
@@ -53,4 +59,41 @@ public class HeapDumpCategory extends SnapshotCategory<HeapDump> {
         DataSourceWindowManager.sharedInstance().openDataSource(new HeapDumpImpl(file, null));
     }
 
+    protected boolean isSnapshot(File file) {
+        if (super.isSnapshot(file)) {
+            return true;
+        }
+        return checkHprofFile(file);
+    }
+
+    private boolean checkHprofFile(File file) {
+        try {
+            if (file.isFile() && file.canRead() && file.length()>MIN_HPROF_SIZE) { // heap dump must be 1M and bigger
+                byte[] prefix = new byte[HPROF_HEADER.length()+4];
+                RandomAccessFile raf = new RandomAccessFile(file,"r");  // NOI18H
+                raf.readFully(prefix);
+                if (new String(prefix).startsWith(HPROF_HEADER)) {
+                    return true;
+                }
+            }
+        } catch (FileNotFoundException ex) {
+            return false;
+        } catch (IOException ex) {
+            return false;
+        }
+        return false;
+    }
+
+    public FileFilter getFileFilter() {
+        return new FileFilter() {
+            public boolean accept(File f) {
+                return f.isDirectory() || isSnapshot(f);
+            }
+            public String getDescription() {
+                String suff = getSuffix();
+                return getName() + (suff != null ? " (*" + suff +", *.*"+ ")" : "");    // NOI18N
+            }
+        };
+    }    
+    
 }
