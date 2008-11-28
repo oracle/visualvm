4717ba985ac16a94acc3c7d419de5b324bf05131
Petr Cyhelsky <cyhelsky@netbeans.org>
2008-11-28 14:19:50 +0100

    Issue #154091 - eliminate unneded instances of various profiling point factories

--- profiler/src/org/netbeans/modules/profiler/ppoints/TimedTakeSnapshotProfilingPointFactory.java
+++ profiler/src/org/netbeans/modules/profiler/ppoints/TimedTakeSnapshotProfilingPointFactory.java
@@ -42,12 +42,11 @@ package org.netbeans.modules.profiler.ppoints;
 
 import org.netbeans.api.project.Project;
 import org.netbeans.api.project.ProjectUtils;
-import org.netbeans.modules.profiler.ppoints.ui.TakeSnapshotCustomizer;
 import org.netbeans.modules.profiler.ppoints.ui.TimedTakeSnapshotCustomizer;
 import org.openide.ErrorManager;
 import org.openide.util.ImageUtilities;
 import org.openide.util.NbBundle;
-import org.openide.util.Utilities;
+import org.openide.util.Lookup;
 import java.text.MessageFormat;
 import java.util.Properties;
 import javax.swing.Icon;
@@ -74,16 +73,11 @@ public class TimedTakeSnapshotProfilingPointFactory extends CodeProfilingPointFa
     public static final Icon TAKE_SNAPSHOT_PP_ICON = new ImageIcon(ImageUtilities.loadImage("org/netbeans/modules/profiler/ppoints/ui/resources/timedTakeSnapshotProfilingPoint.png")); // NOI18N
     public static final String TAKE_SNAPSHOT_PP_TYPE = PP_TYPE;
     public static final String TAKE_SNAPSHOT_PP_DESCR = PP_DESCR;
-    private static TimedTakeSnapshotProfilingPointFactory defaultInstance;
 
     //~ Methods ------------------------------------------------------------------------------------------------------------------
 
     public static TimedTakeSnapshotProfilingPointFactory getDefault() {
-        if (defaultInstance == null) {
-            defaultInstance = new TimedTakeSnapshotProfilingPointFactory();
-        }
-
-        return defaultInstance;
+        return Lookup.getDefault().lookup(TimedTakeSnapshotProfilingPointFactory.class);
     }
 
     public String getDescription() {
