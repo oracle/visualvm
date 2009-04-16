5f95fa85ed27044f4a74ae503a3453abfd6449f0
Jaroslav Tulach <jtulach@netbeans.org>
2009-04-16 13:38:01 +0200

    Keeping original license text and javadoc per review by thurka

--- profiler/src/org/netbeans/modules/profiler/heapwalk/ui/OpenHeapWalkerAction.java
+++ profiler/src/org/netbeans/modules/profiler/heapwalk/ui/OpenHeapWalkerAction.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 2009 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
@@ -21,6 +21,11 @@
  * your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
  *
+ * Contributor(s):
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ *
  * If you wish your version of this file to be governed by only the CDDL
  * or only the GPL Version 2, indicate your decision by adding
  * "[Contributor] elects to include this software in this distribution
@@ -31,10 +36,6 @@
  * However, if you add GPL Version 2 code and therefore, elected the GPL
  * Version 2 license, then the option applies only if the new code is
  * made subject to such option by the copyright holder.
- *
- * Contributor(s):
- *
- * Portions Copyrighted 2009 Sun Microsystems, Inc.
  */
 package org.netbeans.modules.profiler.heapwalk.ui;
 
@@ -47,6 +48,11 @@ import org.netbeans.modules.profiler.utils.IDEUtils;
 import org.openide.util.NbBundle;
 import org.openide.util.RequestProcessor;
 
+/**
+ * Opens the Heap Walker
+ *
+ * @author Jiri Sedlacek
+ */
 public final class OpenHeapWalkerAction implements ActionListener {
     private static File importDir;
 
