8bc2f1f8cc366f575b0a0656766d66af9dc32ad3
Jiri Sedlacek <jis@netbeans.org>
2009-04-24 13:53:57 +0200

    Charts: I18N, mouse wheel click for fixed scale/scale to fit

--- lib.profiler.ui/src/org/netbeans/lib/profiler/ui/charts/xy/ProfilerXYChart.java
+++ lib.profiler.ui/src/org/netbeans/lib/profiler/ui/charts/xy/ProfilerXYChart.java
@@ -1,23 +1,23 @@
 /*
  * Copyright 2007-2008 Sun Microsystems, Inc.  All Rights Reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- * 
+ *
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 only, as
  * published by the Free Software Foundation.  Sun designates this
  * particular file as subject to the "Classpath" exception as provided
  * by Sun in the LICENSE file that accompanied this code.
- * 
+ *
  * This code is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
  * version 2 for more details (a copy is included in the LICENSE file that
  * accompanied this code).
- * 
+ *
  * You should have received a copy of the GNU General Public License version
  * 2 along with this work; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- * 
+ *
  * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa Clara,
  * CA 95054 USA or visit www.sun.com if you need additional information or
  * have any questions.
@@ -31,18 +31,31 @@ import org.netbeans.lib.profiler.charts.ChartComponent;
 import org.netbeans.lib.profiler.charts.ChartConfigurationListener;
 import org.netbeans.lib.profiler.charts.PaintersModel;
 import java.awt.Rectangle;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.ResourceBundle;
 import javax.swing.AbstractAction;
 import javax.swing.Action;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.SwingUtilities;
 
 /**
  *
  * @author Jiri Sedlacek
  */
 public class ProfilerXYChart extends ChartComponent {
+
+    // -----
+    // I18N String constants
+    private static final ResourceBundle messages = ResourceBundle.getBundle("org.netbeans.lib.profiler.ui.charts.xy.Bundle"); // NOI18N
+    private static final String ZOOM_IN_STRING = messages.getString("ProfilerXYChart_ZoomInString"); // NOI18N
+    private static final String ZOOM_OUT_STRING = messages.getString("ProfilerXYChart_ZoomOutString"); // NOI18N
+    private static final String FIXED_SCALE_STRING = messages.getString("ProfilerXYChart_FixedScaleString"); // NOI18N
+    private static final String SCALE_TO_FIT_STRING = messages.getString("ProfilerXYChart_ScaleToFitString"); // NOI18N
+    // -----
 
     private static final Icon ZOOM_IN_ICON =
             new ImageIcon(ProfilerXYChart.class.getResource(
@@ -144,7 +157,7 @@ public class ProfilerXYChart extends ChartComponent {
             double viewX = getViewX(timeline.getTimestamp(index - 1));
 
             if (viewX < viewStart) return new int[] { index, -1 };
-            
+
             index--;
         }
 
@@ -166,7 +179,7 @@ public class ProfilerXYChart extends ChartComponent {
 
             if (viewX < viewStart) return new int[] { -1, index + 1 };
             if (viewX <= viewEnd) return new int[] { index, -1 };
-            
+
             index--;
         }
 
@@ -186,7 +199,7 @@ public class ProfilerXYChart extends ChartComponent {
 
             if (viewX > viewEnd) return new int[] { -1, index - 1 };
             if (viewX >= viewStart) return new int[] { index, -1 };
-            
+
             index++;
         }
 
@@ -286,7 +299,7 @@ public class ProfilerXYChart extends ChartComponent {
         oldOffsetX = newOffsetX;
         visibleIndexesDirty = false;
     }
-    
+
     private int[][] getVisibleBounds(Rectangle viewRect) {
         updateVisibleIndexes();
 
@@ -320,7 +333,7 @@ public class ProfilerXYChart extends ChartComponent {
         int nearestIndex = firstVisibleIndex[0];
         if (nearestIndex == -1) nearestIndex = firstVisibleIndex[1];
         if (nearestIndex == -1) return -1;
-        
+
         long itemDataX = timeline.getTimestamp(nearestIndex);
         long nearestDistance = Math.abs(dataX - itemDataX);
 
@@ -349,7 +362,7 @@ public class ProfilerXYChart extends ChartComponent {
         public ZoomInAction() {
             super();
 
-            putValue(SHORT_DESCRIPTION, "Zoom In");
+            putValue(SHORT_DESCRIPTION, ZOOM_IN_STRING);
             putValue(SMALL_ICON, ZOOM_IN_ICON);
 
             updateAction();
@@ -359,7 +372,7 @@ public class ProfilerXYChart extends ChartComponent {
             boolean followsWidth = currentlyFollowingDataWidth();
             zoom(getWidth() / 2, getHeight() / 2, 2d);
             if (followsWidth) setOffset(getMaxOffsetX(), getOffsetY());
-            
+
             repaintDirty();
         }
 
@@ -377,7 +390,7 @@ public class ProfilerXYChart extends ChartComponent {
         public ZoomOutAction() {
             super();
 
-            putValue(SHORT_DESCRIPTION, "Zoom Out");
+            putValue(SHORT_DESCRIPTION, ZOOM_OUT_STRING);
             putValue(SMALL_ICON, ZOOM_OUT_ICON);
 
             updateAction();
@@ -387,7 +400,7 @@ public class ProfilerXYChart extends ChartComponent {
             boolean followsWidth = currentlyFollowingDataWidth();
             zoom(getWidth() / 2, getHeight() / 2, 0.5d);
             if (followsWidth) setOffset(getMaxOffsetX(), getOffsetY());
-            
+
             repaintDirty();
         }
 
@@ -406,6 +419,12 @@ public class ProfilerXYChart extends ChartComponent {
         public ToggleViewAction() {
             super();
             updateAction();
+            addMouseListener(new MouseAdapter() {
+                public void mouseClicked(MouseEvent e) {
+                    if (SwingUtilities.isMiddleMouseButton(e))
+                        actionPerformed(null);
+                }
+            });
         }
 
         public void actionPerformed(ActionEvent e) {
@@ -419,7 +438,7 @@ public class ProfilerXYChart extends ChartComponent {
             }
 
             setFitsWidth(!fitsWidth);
-            
+
             if (fitsWidth && origOffsetX != -1 && origScaleX != -1) {
                 setScale(origScaleX, getScaleY());
                 setOffset(origOffsetX, getOffsetY());
@@ -428,15 +447,15 @@ public class ProfilerXYChart extends ChartComponent {
             updateAction();
             if (zoomInAction != null) zoomInAction.updateAction();
             if (zoomOutAction != null) zoomOutAction.updateAction();
-            
+
             repaintDirty();
-            
+
         }
 
         private void updateAction() {
             boolean fitsWidth = fitsWidth();
             Icon icon = fitsWidth ? FIXED_SCALE_ICON : SCALE_TO_FIT_ICON;
-            String name = fitsWidth ? "Fixed Scale" : "Scale To Fit";
+            String name = fitsWidth ? FIXED_SCALE_STRING : SCALE_TO_FIT_STRING;
             putValue(SHORT_DESCRIPTION, name);
             putValue(SMALL_ICON, icon);
         }
