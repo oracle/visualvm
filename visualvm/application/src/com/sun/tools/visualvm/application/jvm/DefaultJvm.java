57b8c8982f914800d9c7eb90b30b046e0ed73e78
Tomas Hurka <thurka@java.net>
2009-09-02 06:24:26 +0000

    Jvm.getMonitoredData() added
    git-svn-id: https://svn.java.net/svn/visualvm~svn/trunk@2094 ba0df5e5-9053-6d9f-941f-f112716aa91a

--- visualvm/application/src/com/sun/tools/visualvm/application/jvm/DefaultJvm.java
+++ visualvm/application/src/com/sun/tools/visualvm/application/jvm/DefaultJvm.java
@@ -34,142 +34,146 @@ import java.util.Properties;
  * @author Tomas Hurka
  */
 class DefaultJvm extends Jvm {
-
-
-  DefaultJvm() {
-
-  }
-
-  public boolean is14() {
-    return false;
-  }
-
-  public boolean is15() {
-    return false;
-  }
-
-  public boolean is16() {
-    return false;
-  }
-
-  public boolean is17() {
-    return false;
-  }
-
-  public boolean isAttachable() {
-    return false;
-  }
-
-  public String getCommandLine() {
-    throw new UnsupportedOperationException();
-  }
-
-  public String getJvmArgs() {
-    throw new UnsupportedOperationException();
-  }
-
-  public String getJvmFlags() {
-    throw new UnsupportedOperationException();
-  }
-
-  public String getMainArgs() {
-    throw new UnsupportedOperationException();
-  }
-  
-  public String getMainClass() {
-    throw new UnsupportedOperationException();
-  }
-  
-  public String getVmVersion() {
-    throw new UnsupportedOperationException();
-  }
-  
-  public String getJavaHome() {
-    throw new UnsupportedOperationException();
-  }
-  
-  public String getVmInfo() {
-    throw new UnsupportedOperationException();
-  }
-  
-  public String getVmName() {
-    throw new UnsupportedOperationException();
-  }
-  
-  public String getVmVendor() {
-    throw new UnsupportedOperationException();
-  }
-  
-  public Properties getSystemProperties() {
-    throw new UnsupportedOperationException();
-  }
     
-  public synchronized void addMonitoredDataListener(MonitoredDataListener l) {
-    throw new UnsupportedOperationException();
-  }
-  
-  public synchronized void removeMonitoredDataListener(MonitoredDataListener l) {
-    throw new UnsupportedOperationException();
-  }
-  
-  public boolean isDumpOnOOMEnabled() {
-    throw new UnsupportedOperationException();
-  }
-  
-  public void setDumpOnOOMEnabled(boolean enabled) {
-    throw new UnsupportedOperationException();
-  }
-  
-  public File takeHeapDump() throws IOException {
-    throw new UnsupportedOperationException();
-  }
-  
-  public File takeThreadDump() throws IOException {
-    throw new UnsupportedOperationException();
-  }
-
-  public boolean isBasicInfoSupported() {
-    return false;
-  }
-
-  public boolean isMonitoringSupported() {
-    return isClassMonitoringSupported() || isThreadMonitoringSupported() || isMemoryMonitoringSupported();
-  }
-
-  public boolean isClassMonitoringSupported() {
-    return false;
-  }
-
-  public boolean isThreadMonitoringSupported() {
-    return false;
-  }
-
-  public boolean isMemoryMonitoringSupported() {
-    return false;
-  }
-
-  public boolean isGetSystemPropertiesSupported() {
-    return false;
-  }
-
-  public boolean isDumpOnOOMEnabledSupported() {
-    return false;
-  }
-
-  public boolean isTakeHeapDumpSupported() {
-    return false;
-  }
-
-  public boolean isTakeThreadDumpSupported() {
-    return false;
-  }
-
-  public boolean isCpuMonitoringSupported() {
-    return false;
-  }
-  
-  public boolean isCollectionTimeSupported() {
-    return false;
-  }
-
+    
+    DefaultJvm() {
+        
+    }
+    
+    public boolean is14() {
+        return false;
+    }
+    
+    public boolean is15() {
+        return false;
+    }
+    
+    public boolean is16() {
+        return false;
+    }
+    
+    public boolean is17() {
+        return false;
+    }
+    
+    public boolean isAttachable() {
+        return false;
+    }
+    
+    public String getCommandLine() {
+        throw new UnsupportedOperationException();
+    }
+    
+    public String getJvmArgs() {
+        throw new UnsupportedOperationException();
+    }
+    
+    public String getJvmFlags() {
+        throw new UnsupportedOperationException();
+    }
+    
+    public String getMainArgs() {
+        throw new UnsupportedOperationException();
+    }
+    
+    public String getMainClass() {
+        throw new UnsupportedOperationException();
+    }
+    
+    public String getVmVersion() {
+        throw new UnsupportedOperationException();
+    }
+    
+    public String getJavaHome() {
+        throw new UnsupportedOperationException();
+    }
+    
+    public String getVmInfo() {
+        throw new UnsupportedOperationException();
+    }
+    
+    public String getVmName() {
+        throw new UnsupportedOperationException();
+    }
+    
+    public String getVmVendor() {
+        throw new UnsupportedOperationException();
+    }
+    
+    public Properties getSystemProperties() {
+        throw new UnsupportedOperationException();
+    }
+    
+    public synchronized void addMonitoredDataListener(MonitoredDataListener l) {
+        throw new UnsupportedOperationException();
+    }
+    
+    public synchronized void removeMonitoredDataListener(MonitoredDataListener l) {
+        throw new UnsupportedOperationException();
+    }
+    
+    public boolean isDumpOnOOMEnabled() {
+        throw new UnsupportedOperationException();
+    }
+    
+    public void setDumpOnOOMEnabled(boolean enabled) {
+        throw new UnsupportedOperationException();
+    }
+    
+    public File takeHeapDump() throws IOException {
+        throw new UnsupportedOperationException();
+    }
+    
+    public File takeThreadDump() throws IOException {
+        throw new UnsupportedOperationException();
+    }
+    
+    public boolean isBasicInfoSupported() {
+        return false;
+    }
+    
+    public boolean isMonitoringSupported() {
+        return isClassMonitoringSupported() || isThreadMonitoringSupported() || isMemoryMonitoringSupported();
+    }
+    
+    public boolean isClassMonitoringSupported() {
+        return false;
+    }
+    
+    public boolean isThreadMonitoringSupported() {
+        return false;
+    }
+    
+    public boolean isMemoryMonitoringSupported() {
+        return false;
+    }
+    
+    public boolean isGetSystemPropertiesSupported() {
+        return false;
+    }
+    
+    public boolean isDumpOnOOMEnabledSupported() {
+        return false;
+    }
+    
+    public boolean isTakeHeapDumpSupported() {
+        return false;
+    }
+    
+    public boolean isTakeThreadDumpSupported() {
+        return false;
+    }
+    
+    public boolean isCpuMonitoringSupported() {
+        return false;
+    }
+    
+    public boolean isCollectionTimeSupported() {
+        return false;
+    }
+    
+    public MonitoredData getMonitoredData() {
+        return null;
+    }
+    
 }
