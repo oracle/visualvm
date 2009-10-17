017b0dd9549c6155c594318aa227185736620fc2
Jiri Sedlacek <jsedlacek@java.net>
2009-10-17 17:48:28 +0000

    Fixed Issue 325 - SysTray plugin doesn't work correctly on Linux  - workaround to restore unintentionally minimized window  - spec. version 1.1 -> 1.2
    git-svn-id: https://svn.java.net/svn/visualvm~svn/trunk@2248 ba0df5e5-9053-6d9f-941f-f112716aa91a

--- plugins/systray/src/com/sun/tools/visualvm/modules/systray/SysTray.java
+++ plugins/systray/src/com/sun/tools/visualvm/modules/systray/SysTray.java
@@ -1,23 +1,23 @@
 /*
  * Copyright 2007-2008 Sun Microsystems, Inc.  All Rights Reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- * 
+ *
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 only, as
  * published by the Free Software Foundation.  Sun designates this
  * particular file as subject to the "Classpath" exception as provided
  * by Sun in the LICENSE file that accompanied this code.
- * 
+ *
  * This code is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
  * version 2 for more details (a copy is included in the LICENSE file that
  * accompanied this code).
- * 
+ *
  * You should have received a copy of the GNU General Public License version
  * 2 along with this work; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- * 
+ *
  * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa Clara,
  * CA 95054 USA or visit www.sun.com if you need additional information or
  * have any questions.
@@ -50,6 +50,7 @@ import org.openide.LifecycleManager;
 import org.openide.NotifyDescriptor;
 import org.openide.util.Exceptions;
 import org.openide.util.ImageUtilities;
+import org.openide.util.Utilities;
 import org.openide.windows.WindowManager;
 
 /**
@@ -57,45 +58,47 @@ import org.openide.windows.WindowManager;
  * @author Jiri Sedlacek
  */
 class SysTray {
-    
+
     private static SysTray INSTANCE;
-    
+
     private TrayIcon trayIcon;
     private Frame mainWindow;
     private WindowStateListener mainWindowListener;
-    
+
     private PopupMenu trayPopup;
     private MenuItem showHideItem;
     private MenuItem exitItem;
     private CheckboxMenuItem onTopItem;
     private CheckboxMenuItem hideMinimizedItem;
     private CheckboxMenuItem hideTrayIconItem;
-    
+
     private int lastWindowState;
     private boolean hideWhenMinimized;
     private boolean hideTrayIcon;
-    
-    
+
+    private boolean workaround;
+
+
     static synchronized SysTray getInstance() {
         if (INSTANCE == null) INSTANCE = new SysTray();
         return INSTANCE;
     }
-    
-    
+
+
     synchronized void initialize() {
         if (SystemTray.isSupported()) {
             mainWindow = WindowManager.getDefault().getMainWindow();
             mainWindowListener = new MainWindowListener();
-            
+
             lastWindowState = mainWindow.getExtendedState();
-            
+
             loadSettings();
-            
+
             if (!hideTrayIcon) showTrayIcon();
             mainWindow.addWindowStateListener(mainWindowListener);
         }
     }
-    
+
     synchronized void uninitialize() {
         if (trayIcon != null) hideTrayIcon();
         if (mainWindow != null && mainWindowListener != null) {
@@ -103,15 +106,15 @@ class SysTray {
             mainWindow = null;
         }
     }
-    
-    
+
+
     private void loadSettings() {
         SysTrayPreferences preferences = SysTrayPreferences.getInstance();
         hideWhenMinimized = preferences.getHideWhenMinimized();
         hideTrayIcon = preferences.getHideTrayIcon();
     }
-    
-    
+
+
     private void showTrayIcon() {
         try {
             SystemTray tray = SystemTray.getSystemTray();
@@ -130,7 +133,7 @@ class SysTray {
             Exceptions.printStackTrace(e);
         }
     }
-    
+
     private void hideTrayIcon() {
         SystemTray tray = SystemTray.getSystemTray();
         if (tray != null) {
@@ -142,15 +145,15 @@ class SysTray {
         }
         trayIcon = null;
     }
-    
-    
+
+
     private TrayIcon createTrayIcon() {
         Image image = createTrayImage();
         String tooltip = createTrayTooltip();
         trayPopup = createTrayPopup();
         TrayIcon icon = new TrayIcon(image, tooltip, trayPopup);
         icon.setImageAutoSize(true);
-        
+
         icon.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 SwingUtilities.invokeLater(new Runnable() {
@@ -160,30 +163,30 @@ class SysTray {
                 });
             }
         });
-        
+
         return icon;
     }
-    
+
     private String createTrayTooltip() {
         return mainWindow.getTitle();
     }
-    
+
     private Image createTrayImage() {
         Dimension iconDimension = SystemTray.getSystemTray().getTrayIconSize();
         int iconWidth = iconDimension.width;
         int iconHeight = iconDimension.height;
-        
+
         if (iconWidth <= 16 && iconHeight <= 16)
             return ImageUtilities.loadImage("com/sun/tools/visualvm/modules/systray/resources/icon16.png"); // NOI18N
-        
+
         if (iconWidth <= 32 && iconHeight <= 32)
             return ImageUtilities.loadImage("com/sun/tools/visualvm/modules/systray/resources/icon32.png"); // NOI18N
-        
+
         return ImageUtilities.loadImage("com/sun/tools/visualvm/modules/systray/resources/icon48.png"); // NOI18N
     }
-    
+
     private PopupMenu createTrayPopup() {
-        
+
         // "Show / Hide" menu item
         showHideItem = new MenuItem(mainWindow.isVisible() ? "Hide" : "Show");
         showHideItem.setFont(UIManager.getFont("MenuItem.font").deriveFont(Font.BOLD)); // NOI18N
@@ -196,7 +199,7 @@ class SysTray {
                 });
             }
         });
-        
+
         // "Always on top" menu item
         if (Toolkit.getDefaultToolkit().isAlwaysOnTopSupported() && mainWindow.isAlwaysOnTopSupported()) {
             onTopItem = new CheckboxMenuItem("Always on top", SysTrayPreferences.getInstance().getAlwaysOnTop());
@@ -212,7 +215,7 @@ class SysTray {
             });
             toggleAlwaysOnTop(); // Sets initial state
         }
-        
+
         // "Hide when minimized" menu item
         hideMinimizedItem = new CheckboxMenuItem("Hide when minimized", hideWhenMinimized);
         hideMinimizedItem.setFont(UIManager.getFont("MenuItem.font")); // NOI18N
@@ -225,7 +228,7 @@ class SysTray {
                 });
             }
         });
-        
+
         // "No trayicon when showing" menu item
         hideTrayIconItem = new CheckboxMenuItem("No trayicon when showing", hideTrayIcon);
         hideTrayIconItem.setFont(UIManager.getFont("MenuItem.font")); // NOI18N
@@ -238,7 +241,7 @@ class SysTray {
                 });
             }
         });
-        
+
         // "Exit" menu item
         exitItem = new MenuItem("Exit");
         exitItem.setFont(UIManager.getFont("MenuItem.font")); // NOI18N
@@ -251,15 +254,15 @@ class SysTray {
                 });
             }
         });
-        
+
         // "Settings" submenu
         PopupMenu settingsItem = new PopupMenu("Settings");
         settingsItem.setFont(UIManager.getFont("MenuItem.font")); // NOI18N
         if (onTopItem != null) settingsItem.add(onTopItem);
         settingsItem.add(hideMinimizedItem);
         settingsItem.add(hideTrayIconItem);
-        
-        
+
+
         PopupMenu popupMenu = new PopupMenu();
         popupMenu.add(showHideItem);
         popupMenu.add(settingsItem);
@@ -267,12 +270,12 @@ class SysTray {
         popupMenu.add(exitItem);
         return popupMenu;
     }
-    
+
     private void toggleWindowVisibility() {
         if (mainWindow.isVisible()) hideWindow(); // May not hide window when modal dialog(s) in the way
         else showWindow();
     }
-    
+
     private void hideWindow() {
         Window[] windows = mainWindow.getOwnedWindows();
         for (Window window : windows) {
@@ -284,30 +287,33 @@ class SysTray {
                     return;
                 }
         }
-        
+
         mainWindow.setVisible(false);
+        if (!Utilities.isWindows() && (mainWindow.getExtendedState() & Frame.ICONIFIED) != 0) {
+            workaround = true;
+        }
         if (showHideItem != null) showHideItem.setLabel("Show");
     }
-    
+
     private void showWindow() {
         mainWindow.setVisible(true);
         mainWindow.setExtendedState(lastWindowState);
         showHideItem.setLabel("Hide");
         mainWindow.toFront();
     }
-    
+
     private void toggleAlwaysOnTop() {
         mainWindow.setAlwaysOnTop(onTopItem.getState());
         SysTrayPreferences.getInstance().setAlwaysOnTop(onTopItem.getState());
     }
-    
+
     private void toggleHideWhenMinimized() {
         hideWhenMinimized = hideMinimizedItem.getState();
         if (hideWhenMinimized && (mainWindow.getExtendedState() & Frame.ICONIFIED) != 0)
             hideWindow(); // May not hide window when modal dialog(s) in the way
         SysTrayPreferences.getInstance().setHideWhenMinimized(hideWhenMinimized);
     }
-    
+
     private void toggleNoShowingIcon() {
         hideTrayIcon = hideTrayIconItem.getState();
         int windowState = mainWindow.getExtendedState();
@@ -318,19 +324,24 @@ class SysTray {
         }
         SysTrayPreferences.getInstance().setHideTrayIcon(hideTrayIcon);
     }
-    
-    
-    private SysTray() {        
-    }
-    
-    
+
+
+    private SysTray() {}
+
+
     private class MainWindowListener implements WindowStateListener {
-        
+
         public void windowStateChanged(WindowEvent e) {
             int windowState = e.getNewState();
             if ((windowState & Frame.ICONIFIED) != 0) {
-                if (hideWhenMinimized || hideTrayIcon) hideWindow(); // May not hide window when modal dialog(s) in the way
-                if (!mainWindow.isVisible() && hideTrayIcon && trayIcon == null) showTrayIcon();
+                if (workaround) {
+                    workaround = false;
+                    mainWindow.setExtendedState(lastWindowState);
+                } else {
+                    workaround = false;
+                    if (hideWhenMinimized || hideTrayIcon) hideWindow(); // May not hide window when modal dialog(s) in the way
+                    if (!mainWindow.isVisible() && hideTrayIcon && trayIcon == null) showTrayIcon();
+                }
             } else {
                 lastWindowState = windowState;
                 if (hideTrayIcon && trayIcon != null) hideTrayIcon();
