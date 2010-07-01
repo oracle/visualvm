eb02089623fcca3fb5ff66e1358d24c3959ad10c
Jaroslav Bachorik <yardus@java.net>
2010-07-01 15:22:57 +0000

    updating license and some module info
    git-svn-id: https://svn.java.net/svn/visualvm~svn/trunk@2667 ba0df5e5-9053-6d9f-941f-f112716aa91a

--- plugins/tracerjavase/src/com/sun/tools/visualvm/modules/tracer/javase/resources/platform_mx.js
+++ plugins/tracerjavase/src/com/sun/tools/visualvm/modules/tracer/javase/resources/platform_mx.js
@@ -1,13 +1,32 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/*
+ *  Copyright 2007-2010 Sun Microsystems, Inc.  All Rights Reserved.
+ *  DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ *  This code is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License version 2 only, as
+ *  published by the Free Software Foundation.  Sun designates this
+ *  particular file as subject to the "Classpath" exception as provided
+ *  by Sun in the LICENSE file that accompanied this code.
+ *
+ *  This code is distributed in the hope that it will be useful, but WITHOUT
+ *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ *  version 2 for more details (a copy is included in the LICENSE file that
+ *  accompanied this code).
+ *
+ *  You should have received a copy of the GNU General Public License version
+ *  2 along with this work; if not, write to the Free Software Foundation,
+ *  Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ *  Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa Clara,
+ *  CA 95054 USA or visit www.sun.com if you need additional information or
+ *  have any questions.
  */
 
 var scriptPath = "nbres:/com/sun/tools/visualvm/modules/tracer/javase/resources/JavaIOTracer.btrace"
 var btraceDeployer = typeof(Packages.net.java.btrace.visualvm.tracer.deployer.BTraceDeployer) == "function" ?
                         Packages.net.java.btrace.visualvm.tracer.deployer.BTraceDeployer.instance() : undefined;
 
-//var img = Packages.org.openide.util.ImageUtilities.loadImageIcon("com/sun/tools/visualvm/modules/tracer/platformjmx/resources/",false)
 
 var Format_KBPS = new Packages.com.sun.tools.visualvm.modules.tracer.dynamic.impl.ItemValueFormatterInterface({
     formatValue: function (value, format) {
