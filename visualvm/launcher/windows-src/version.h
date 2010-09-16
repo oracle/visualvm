fcb34a139f968fecabead6dab79ec8a4c370da89
Tomas Hurka <thurka@java.net>
2010-09-16 13:15:52 +0000

    Copyright updated
    git-svn-id: https://svn.java.net/svn/visualvm~svn/trunk@2840 ba0df5e5-9053-6d9f-941f-f112716aa91a

--- visualvm/launcher/windows-src/version.h
+++ visualvm/launcher/windows-src/version.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2007-2008 Sun Microsystems, Inc.  All Rights Reserved.
+ * Copyright 2007-2010 Sun Microsystems, Inc.  All Rights Reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -23,13 +23,13 @@
  * have any questions.
  */
 
-#define COMPANY "Sun Microsystems, Inc."
+#define COMPANY "Oracle Corporation"
 #define COMPONENT "VisualVM"
 #define VER "0.0.0.0"
 #define FVER 0,0,0,0
 #define BUILD_ID "0"
 #define INTERNAL_NAME "visualvm"
-#define COPYRIGHT "\xA9 2007-2009 Sun Microsystems, Inc."
+#define COPYRIGHT "\xA9 2007, 2010 Oracle and/or its affiliates. All rights reserved."
 #define FNAME "visualvm.exe"
 #define NAME "VisualVM Dev. build"
 
