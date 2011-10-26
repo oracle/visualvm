542ec45cf27fad65b97dfccea110289ef09d3655
Tomas Hurka <thurka@netbeans.org>
2011-10-26 16:15:04 +0200

    bugfix #204102, initial support for JDK 1.8 added

--- lib.profiler/src/org/netbeans/lib/profiler/instrumentation/CodeRegionMethodInstrumentor.java
+++ lib.profiler/src/org/netbeans/lib/profiler/instrumentation/CodeRegionMethodInstrumentor.java
@@ -105,6 +105,9 @@ public class CodeRegionMethodInstrumentor extends ClassManager {
         } else if (status.targetJDKVersionString.equals(JDK_17_STRING)) {
             // for 1.7 we use 15-style instrumentation
             return createInstrumentedMethodPack15();
+        } else if (status.targetJDKVersionString.equals(JDK_18_STRING)) {
+            // for 1.8 we use 15-style instrumentation
+            return createInstrumentedMethodPack15();
         } else {
             throw new InternalError("Unsupported JDK version"); // NOI18N
         }
