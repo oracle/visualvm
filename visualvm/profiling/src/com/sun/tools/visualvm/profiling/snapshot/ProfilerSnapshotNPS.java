67e82d432ccda125f4cd9f041b080598ad523678
Tomas Hurka <thurka@java.net>
2012-10-01 14:47:43 +0000

    toolbar already has the correct opaque
    git-svn-id: https://svn.java.net/svn/visualvm~svn/trunk@3103 ba0df5e5-9053-6d9f-941f-f112716aa91a

--- visualvm/profiling/src/com/sun/tools/visualvm/profiling/snapshot/ProfilerSnapshotNPS.java
+++ visualvm/profiling/src/com/sun/tools/visualvm/profiling/snapshot/ProfilerSnapshotNPS.java
@@ -106,14 +106,6 @@ final class ProfilerSnapshotNPS extends ProfilerSnapshot {
             try {
                 JComponent cpuResPanel = (JComponent) srw.getComponent(0);
                 cpuResPanel.setOpaque(false);
-                JToolBar toolBar = (JToolBar) cpuResPanel.getComponent(1);
-                toolBar.setOpaque(false);
-                int componentsCount = toolBar.getComponentCount();
-                ((JComponent) toolBar.getComponent(0)).setOpaque(false);
-                ((JComponent) toolBar.getComponent(1)).setOpaque(false);
-                ((JComponent) toolBar.getComponent(componentsCount - 3)).setOpaque(false);
-                ((JComponent) toolBar.getComponent(componentsCount - 2)).setOpaque(false);
-                ((JComponent) toolBar.getComponent(componentsCount - 1)).setOpaque(false);
                 JTabbedPane tabbedPane = (JTabbedPane) cpuResPanel.getComponent(0);
                 JComponent infoPanel = (JComponent) tabbedPane.getComponentAt(tabbedPane.getTabCount() - 1);
                 infoPanel.setBorder(BorderFactory.createEmptyBorder());
