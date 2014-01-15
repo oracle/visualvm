a2f96e7b3896d5e31884eacfeaeb56e137232365
Tomas Hurka <thurka@netbeans.org>
2014-01-15 13:34:13 +0100

    compile Windows version of jdk6 native profiler agent with secure runtime flags

--- lib.profiler/native/build/buildnative-windows64-16.bat
+++ lib.profiler/native/build/buildnative-windows64-16.bat
@@ -62,7 +62,7 @@ cl /I%BUILD_JDK%\include /I%BUILD_JDK%\include\win32 ^
 
 rc /fo version.res %BUILD_SRC_15%\windows\version.rc
 
-link /DLL /MAP:%BUILD_OUTPUT%\deployed\jdk16\windows-amd64\profilerinterface.map /OUT:%BUILD_OUTPUT%\deployed\jdk16\windows-amd64\profilerinterface.dll ^
+link /DLL /NXCOMPAT /DYNAMICBASE /MAP:%BUILD_OUTPUT%\deployed\jdk16\windows-amd64\profilerinterface.map /OUT:%BUILD_OUTPUT%\deployed\jdk16\windows-amd64\profilerinterface.dll ^
 Classes.obj HeapDump.obj Timers.obj GC.obj Threads.obj Stacks.obj common_functions.obj class_file_cache.obj attach.obj version.res
 
 del vc60.pdb
