f04a7aec0cff29ec29e03d058b733a11e09229d4
Tomas Hurka <thurka@netbeans.org>
2014-09-15 14:32:47 +0200

    NOI18N

--- profiler.heapwalker/src/org/netbeans/modules/profiler/heapwalk/OQLController.java
+++ profiler.heapwalker/src/org/netbeans/modules/profiler/heapwalk/OQLController.java
@@ -217,7 +217,9 @@ public class OQLController extends AbstractTopLevelController
                             });
 
                             if (counter.get() == 0) {
-                                sb.append("<tr><td><h4>Too many results. Please, refine your query.</h4></td></tr>" );
+                                sb.append("<tr><td><h4>");  // NOI18N
+                                sb.append(NbBundle.getMessage(OQLController.class, "OQL_TOO_MANY_RESULTS_MSG"));      // NOI18N
+                                sb.append("</h4></td></tr>");   // NOI18N
                             } else if (counter.get() == RESULTS_LIMIT) {
                                 sb.append("<tr><td><h4>"); // NOI18N
                                 sb.append(NbBundle.getMessage(OQLController.class, "OQL_NO_RESULTS_MSG")); // NOI18N
