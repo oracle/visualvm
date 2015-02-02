abb37d80b17853e930fb36e399f4e5e252893dba
Tomas Hurka <thurka@netbeans.org>
2015-02-02 16:12:58 +0100

    replace 'java/lang/Object' string with constant

--- lib.profiler/src/org/netbeans/lib/profiler/instrumentation/RecursiveMethodInstrumentor1.java
+++ lib.profiler/src/org/netbeans/lib/profiler/instrumentation/RecursiveMethodInstrumentor1.java
@@ -437,7 +437,7 @@ public class RecursiveMethodInstrumentor1 extends RecursiveMethodInstrumentor {
 
             if (clazz.isMethodNative(idx) || clazz.isMethodAbstract(idx)
                     || (!clazz.isMethodRoot(idx) && !clazz.isMethodMarker(idx) && !instrFilter.passesFilter(className))
-                    || (className == "java/lang/Object")) { // NOI18N  // Actually, just the Object.<init> method?
+                    || (className == OBJECT_SLASHED_CLASS_NAME)) {  // Actually, just the Object.<init> method?
                 clazz.setMethodUnscannable(idx);
             } else {
                 byte[] bytecode = clazz.getMethodBytecode(idx);
