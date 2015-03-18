958f34882f2883296d67268aaacb6d8a9f99580d
Tomas Hurka <thurka@netbeans.org>
2015-03-18 19:45:46 +0100

    when CPU sampling and lock contention monitoring is running together, some threads can be registered twice - do not override old info

--- lib.profiler/src/org/netbeans/lib/profiler/results/cpu/ThreadInfos.java
+++ lib.profiler/src/org/netbeans/lib/profiler/results/cpu/ThreadInfos.java
@@ -115,9 +115,11 @@ class ThreadInfos {
                 threadClassNames = newClassNames;
                 threadInfosLastIdx = threadId;
             }
-            threadInfos[threadId] = new ThreadInfo(threadId);
-            threadNames[threadId] = threadName;
-            threadClassNames[threadId] = threadClassName;
+            if (threadInfos[threadId] == null) {
+                threadInfos[threadId] = new ThreadInfo(threadId);
+                threadNames[threadId] = threadName;
+                threadClassNames[threadId] = threadClassName;
+            }
         } finally {
             endTrans();
         }
