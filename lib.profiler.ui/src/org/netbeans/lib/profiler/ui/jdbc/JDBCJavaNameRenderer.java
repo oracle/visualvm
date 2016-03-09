390d7901ca4f37a2ec8b068940f59f60b9988ee5
Jiri Sedlacek <jis@netbeans.org>
2016-03-09 15:24:10 +0100

    Naive formatting of SQL queries - temporary hack

--- lib.profiler.ui/src/org/netbeans/lib/profiler/ui/jdbc/JDBCJavaNameRenderer.java
+++ lib.profiler.ui/src/org/netbeans/lib/profiler/ui/jdbc/JDBCJavaNameRenderer.java
@@ -44,6 +44,8 @@
 package org.netbeans.lib.profiler.ui.jdbc;
 
 import javax.swing.Icon;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
 import javax.swing.UIManager;
 import org.netbeans.lib.profiler.results.memory.PresoObjAllocCCTNode;
 import org.netbeans.lib.profiler.ui.swing.renderer.JavaNameRenderer;
@@ -64,6 +66,12 @@ public class JDBCJavaNameRenderer extends JavaNameRenderer {
     private final Icon icon;
     private final Icon iconDisabled;
     
+    private final JLabel sqlRenderer = new JLabel();
+    
+    private String currentValue;
+    
+    private boolean isSQL;
+    
     public JDBCJavaNameRenderer() {
         this(ProfilerIcons.NODE_REVERSE);
     }
@@ -77,24 +85,64 @@ public class JDBCJavaNameRenderer extends JavaNameRenderer {
         if (value instanceof PresoObjAllocCCTNode) {
             PresoObjAllocCCTNode node = (PresoObjAllocCCTNode)value;
             
-            if (node.isFiltered()) {
-                setNormalValue(""); // NOI18N
-                setBoldValue(""); // NOI18N
-                setGrayValue(node.getNodeName());
-            } else {
-                super.setValue(node.getNodeName(), row);
-            }
+            boolean filtered = node.isFiltered();
+            currentValue = node.getNodeName();
             
-            if (JDBCTreeTableView.isSQL(node)) {
-                setIcon(node.isFiltered() ? SQL_ICON_DISABLED : SQL_ICON);
-            } else if (node.isLeaf()) {
-                setIcon(node.isFiltered() ? LEAF_ICON_DISABLED : LEAF_ICON);
+            isSQL = JDBCTreeTableView.isSQL(node);
+            if (isSQL) {
+                sqlRenderer.setText(formatSQLCommand(currentValue));
+                sqlRenderer.setIcon(filtered ? SQL_ICON_DISABLED : SQL_ICON);
             } else {
-                setIcon(node.isFiltered() ? iconDisabled : icon);
+                if (filtered) {
+                    setNormalValue(""); // NOI18N
+                    setBoldValue(""); // NOI18N
+                    setGrayValue(currentValue);
+                } else {
+                    super.setValue(currentValue, row);
+                }
+
+                if (node.isLeaf()) {
+                    setIcon(filtered ? LEAF_ICON_DISABLED : LEAF_ICON);
+                } else {
+                    setIcon(filtered ? iconDisabled : icon);
+                }
             }
         } else {
             super.setValue(value, row);
         }
+    }
+    
+    public JComponent getComponent() {
+        return isSQL ? sqlRenderer : super.getComponent();
+    }
+    
+    public String toString() {
+        return currentValue;
+    }
+    
+    
+    private static String formatSQLCommand(String command) {
+        StringBuilder s = new StringBuilder();
+        s.append("<html>"); // NOI18N
+        
+        command = command.replace("CREATE TABLE ", "<b>CREATE TABLE </b>"); // NOI18N
+        command = command.replace("ALTER TABLE ", "<b>ALTER TABLE </b>"); // NOI18N
+        command = command.replace("TRUNCATE TABLE ", "<b>TRUNCATE TABLE </b>"); // NOI18N
+        command = command.replace("INSERT INTO ", "<b>INSERT INTO </b>"); // NOI18N
+//        command = command.replace("DELETE FROM ", "<b>DELETE FROM </b>"); // NOI18N
+        command = command.replace("SELECT ", "<b>SELECT </b>"); // NOI18N
+        command = command.replace("DELETE ", "<b>DELETE </b>"); // NOI18N 
+        command = command.replace("FROM ", "<b>FROM </b>"); // NOI18N
+        command = command.replace("WHERE ", "<b>WHERE </b>"); // NOI18N
+        command = command.replace("UPDATE ", "<b>UPDATE </b>"); // NOI18N
+        command = command.replace("VALUES ", "<b>VALUES </b>"); // NOI18N
+        command = command.replace("DISTINCT ", "<b>DISTINCT </b>"); // NOI18N
+        command = command.replace("ORDER BY ", "<b>ORDER BY </b>"); // NOI18N
+        command = command.replace("GROUP BY ", "<b>GROUP BY </b>"); // NOI18N
+        s.append(command);
+        
+        s.append("</html>"); // NOI18N
+        return s.toString();
     }
     
 }
