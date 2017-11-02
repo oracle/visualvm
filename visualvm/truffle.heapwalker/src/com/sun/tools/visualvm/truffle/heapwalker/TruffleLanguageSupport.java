754ab9b0cbc8ab20e3f19710da75970321613edd
Tomas Hurka <tomas.hurka@gmail.com>
2017-11-02 17:36:32 +0100

    GR-6734 make sure LanguageInfo class has name field - SVM can remove it

--- visualvm/truffle.heapwalker/src/com/sun/tools/visualvm/truffle/heapwalker/TruffleLanguageSupport.java
+++ visualvm/truffle.heapwalker/src/com/sun/tools/visualvm/truffle/heapwalker/TruffleLanguageSupport.java
@@ -26,6 +26,7 @@ package com.sun.tools.visualvm.truffle.heapwalker;
 
 import java.io.IOException;
 import java.util.List;
+import org.netbeans.lib.profiler.heap.Field;
 import org.netbeans.lib.profiler.heap.Heap;
 import org.netbeans.lib.profiler.heap.Instance;
 import org.netbeans.lib.profiler.heap.JavaClass;
@@ -39,6 +40,7 @@ public class TruffleLanguageSupport {
     
     private static final String LANGUAGE_INFO_FQN = "com.oracle.truffle.api.nodes.LanguageInfo"; // NOI18N
     private static final String LANGUAGE_CACHE_FQN = "com.oracle.truffle.api.vm.LanguageCache"; // NOI18N
+    private static final String NAME_FIELD = "name";    // NOI18N
 
     public static Instance getLanguageInfo(Heap heap, String languageID) throws IOException {
         // check for DynamicObject
@@ -48,9 +50,9 @@ public class TruffleLanguageSupport {
         
         // check for LanguageInfo
         JavaClass langInfoClass = heap.getJavaClassByName(LANGUAGE_INFO_FQN);
-        if (langInfoClass == null) {
+        if (!checkLangClass(langInfoClass)) {
             langInfoClass = heap.getJavaClassByName(LANGUAGE_CACHE_FQN);
-            if (langInfoClass == null) {
+            if (!checkLangClass(langInfoClass)) {
                 return null;
             }
         }
@@ -69,4 +71,16 @@ public class TruffleLanguageSupport {
         return null;
     }
     
+    static boolean checkLangClass(JavaClass infoClass) {
+        if (infoClass != null) {
+            for (Object f : infoClass.getFields()) {
+                Field field = (Field) f;
+
+                if (NAME_FIELD.equals(field.getName())) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
