15dee7f3b8629c2882f606cf1b56784959ece01b
Tomas Hurka <tomas.hurka@gmail.com>
2018-02-27 14:20:23 +0100

    profiler modules added to VisualVM suite

--- visualvm/build-nb-ml.sh
+++ visualvm/build-nb-ml.sh
@@ -2,7 +2,7 @@
 
 REV=1132e7bb80b0b3560
 REV_L10N=1180ea1ceb30
-ZIPNAME=nb90_visualvm_`date "+%d%m%Y"`
+ZIPNAME=nb90_platform_`date "+%d%m%Y"`
 
 set -e
 
@@ -32,47 +32,18 @@ cd ..
 git checkout -f $REV
 git status
 
-PROFILER_LIGHT=-Dnb.cluster.profiler.depends=nb.cluster.platform
-PROFILER_LIGHT="$PROFILER_LIGHT -Dnb.cluster.profiler=\
-lib.profiler,\
-lib.profiler.charts,\
-lib.profiler.common,\
-lib.profiler.ui,\
-profiler,\
-profiler.api,\
-profiler.attach,\
-profiler.heapwalker,\
-profiler.oql,\
-profiler.snaptracer,\
-profiler.utilities"
-PROFILER_LIGHT="$PROFILER_LIGHT -Dvalidation.nb.cluster.profiler=profiler"
-
 OPTS=-Dbuild.compiler.debuglevel=source,lines
 
 cd nbbuild
 ant clean
 ant $OPTS -Dname=platform rebuild-cluster
 ant $OPTS -Dname=harness rebuild-cluster
-ant $OPTS -Dname=profiler rebuild-cluster $PROFILER_LIGHT
-
-# cleanup remote packs and cvm support
-rm -rf netbeans/ide/
-rm -rf netbeans/profiler/remote-pack-defs/
-rm -rf netbeans/profiler/lib/deployed/cvm/
-rm -f netbeans/profiler/lib/jfluid-server-cvm.jar
 
 zip -r $BUILD_ROOT/$ZIPNAME.zip netbeans
 
 ant clean
 ant $OPTS -Dlocales=ja,zh_CN -Dname=platform rebuild-cluster
 ant $OPTS -Dlocales=ja,zh_CN -Dname=harness rebuild-cluster
-ant $OPTS -Dlocales=ja,zh_CN -Dname=profiler rebuild-cluster $PROFILER_LIGHT
-
-# cleanup remote packs and cvm support
-rm -rf netbeans/ide/
-rm -rf netbeans/profiler/remote-pack-defs/
-rm -rf netbeans/profiler/lib/deployed/cvm/
-rm -f netbeans/profiler/lib/jfluid-server-cvm.jar
 
 zip -r $BUILD_ROOT/$ZIPNAME-ml.zip netbeans
 
