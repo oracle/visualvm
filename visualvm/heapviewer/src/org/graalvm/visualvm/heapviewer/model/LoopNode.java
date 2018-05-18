4e97011fab8fb18f4259a00751ae369366b0a746
Jiri Sedlacek <jiri.sedlacek@oracle.com>
2018-05-18 10:50:49 +0200

    Fix missing Open in New Tab action on 'loop to' node

--- visualvm/heapviewer/src/org/graalvm/visualvm/heapviewer/model/LoopNode.java
+++ visualvm/heapviewer/src/org/graalvm/visualvm/heapviewer/model/LoopNode.java
@@ -45,6 +45,10 @@ public class LoopNode extends HeapViewerNode {
         this.loopOrigin = loopOrigin;
         setChildren(NO_NODES);
     }
+    
+    public HeapViewerNode createCopy() {
+        return loopNode.createCopy();
+    }
 
     public String toString() {
         return Bundle.LoopNode_Name(loopNode.toString(), loopOrigin.toString());
