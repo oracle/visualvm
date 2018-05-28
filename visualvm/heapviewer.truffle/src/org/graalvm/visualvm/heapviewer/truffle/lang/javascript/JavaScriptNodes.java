1a347137f3c6d0c20b08e201bc2baa3ff7990f16
Tomas Hurka <tomas.hurka@gmail.com>
2018-05-28 16:36:09 +0200

    try DynamicObject.getType() if 'type' is not recognized

--- visualvm/heapviewer.truffle/src/org/graalvm/visualvm/heapviewer/truffle/lang/javascript/JavaScriptNodes.java
+++ visualvm/heapviewer.truffle/src/org/graalvm/visualvm/heapviewer/truffle/lang/javascript/JavaScriptNodes.java
@@ -24,35 +24,37 @@
  */
 package org.graalvm.visualvm.heapviewer.truffle.lang.javascript;
 
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import javax.swing.Icon;
 import org.graalvm.visualvm.heapviewer.HeapContext;
 import org.graalvm.visualvm.heapviewer.model.HeapViewerNode;
-import org.graalvm.visualvm.heapviewer.truffle.nodes.TruffleLocalObjectNode;
-import org.graalvm.visualvm.heapviewer.truffle.nodes.TruffleObjectArrayItemNode;
-import org.graalvm.visualvm.heapviewer.truffle.nodes.TruffleObjectFieldNode;
-import java.util.List;
-import org.graalvm.visualvm.lib.jfluid.heap.FieldValue;
-import org.graalvm.visualvm.lib.jfluid.heap.Heap;
-import org.graalvm.visualvm.lib.jfluid.heap.Instance;
-import org.graalvm.visualvm.lib.jfluid.heap.ObjectFieldValue;
-import org.graalvm.visualvm.lib.ui.Formatters;
-import org.graalvm.visualvm.lib.profiler.heapwalk.details.api.DetailsSupport;
+import org.graalvm.visualvm.heapviewer.truffle.dynamicobject.DynamicObject;
 import org.graalvm.visualvm.heapviewer.truffle.dynamicobject.DynamicObjectArrayItemNode;
 import org.graalvm.visualvm.heapviewer.truffle.dynamicobject.DynamicObjectFieldNode;
 import org.graalvm.visualvm.heapviewer.truffle.dynamicobject.DynamicObjectNode;
 import org.graalvm.visualvm.heapviewer.truffle.dynamicobject.DynamicObjectReferenceNode;
 import org.graalvm.visualvm.heapviewer.truffle.dynamicobject.LocalDynamicObjectNode;
+import org.graalvm.visualvm.heapviewer.truffle.nodes.TruffleLocalObjectNode;
+import org.graalvm.visualvm.heapviewer.truffle.nodes.TruffleObjectArrayItemNode;
+import org.graalvm.visualvm.heapviewer.truffle.nodes.TruffleObjectFieldNode;
 import org.graalvm.visualvm.heapviewer.truffle.nodes.TruffleObjectNode;
 import org.graalvm.visualvm.heapviewer.truffle.nodes.TruffleObjectReferenceNode;
 import org.graalvm.visualvm.heapviewer.truffle.nodes.TruffleOpenNodeActionProvider;
 import org.graalvm.visualvm.heapviewer.truffle.nodes.TruffleTypeNode;
 import org.graalvm.visualvm.heapviewer.ui.HeapViewerNodeAction;
 import org.graalvm.visualvm.heapviewer.ui.HeapViewerRenderer;
-import java.util.Date;
-import java.util.Map;
-import javax.swing.Icon;
 import org.graalvm.visualvm.lib.jfluid.heap.ArrayItemValue;
+import org.graalvm.visualvm.lib.jfluid.heap.FieldValue;
+import org.graalvm.visualvm.lib.jfluid.heap.Heap;
+import org.graalvm.visualvm.lib.jfluid.heap.Instance;
+import org.graalvm.visualvm.lib.jfluid.heap.ObjectFieldValue;
 import org.graalvm.visualvm.lib.profiler.api.icons.Icons;
 import org.graalvm.visualvm.lib.profiler.api.icons.LanguageIcons;
+import org.graalvm.visualvm.lib.profiler.heapwalk.details.api.DetailsSupport;
+import org.graalvm.visualvm.lib.ui.Formatters;
 import org.openide.util.lookup.ServiceProvider;
 
 /**
@@ -139,6 +141,12 @@ public class JavaScriptNodes extends TruffleOpenNodeActionProvider<JavaScriptObj
                 } else {
                     logicalValue = valueField.getValue();
                 }
+            }
+        } else {
+            String rawType = DynamicObject.getType(object.getInstance(), heap);
+
+            if (!Objects.equals(type, rawType)) {
+                return getLogicalValue(object, rawType, heap);
             }
         }
         
