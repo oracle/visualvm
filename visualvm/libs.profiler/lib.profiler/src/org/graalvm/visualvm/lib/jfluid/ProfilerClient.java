6e204b3b7a183b7d3a7f6b5f3103c994c56c5d49
Tomas Hurka <tomas.hurka@gmail.com>
2019-01-23 10:47:27 +0100

    minor code simplification

--- visualvm/libs.profiler/lib.profiler/src/org/graalvm/visualvm/lib/jfluid/ProfilerClient.java
+++ visualvm/libs.profiler/lib.profiler/src/org/graalvm/visualvm/lib/jfluid/ProfilerClient.java
@@ -1643,12 +1643,10 @@ public class ProfilerClient implements CommonConstants {
         // But avoid doing that while we are processing the data at the client side, since it looks like when these two
         // things happen at the same time, it's likely to cause problems.
         // This is a quick fix. Probably a more solid solution is needed.
-        if (handlingEventBufferDump) {
-            while (handlingEventBufferDump) {
-                try {
-                    Thread.sleep(20);
-                } catch (Exception ex) {
-                }
+        while (handlingEventBufferDump) {
+            try {
+                Thread.sleep(20);
+            } catch (Exception ex) {
             }
         }
 
