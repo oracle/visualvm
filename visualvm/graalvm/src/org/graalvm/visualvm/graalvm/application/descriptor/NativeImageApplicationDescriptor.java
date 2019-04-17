a2791576ecbf2ad41ca42024bdcb0825f53594ea
Jiri Sedlacek <jiri.sedlacek@oracle.com>
2019-04-17 16:39:32 +0200

    Commented-out implementation of [native image] sufix for SVM processes

--- visualvm/graalvm/src/org/graalvm/visualvm/graalvm/application/descriptor/NativeImageApplicationDescriptor.java
+++ visualvm/graalvm/src/org/graalvm/visualvm/graalvm/application/descriptor/NativeImageApplicationDescriptor.java
@@ -1,47 +1,44 @@
 /*
- * Copyright (c) 2007, 2011, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
+ * 
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 only, as
  * published by the Free Software Foundation.  Oracle designates this
  * particular file as subject to the "Classpath" exception as provided
  * by Oracle in the LICENSE file that accompanied this code.
- *
+ * 
  * This code is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
  * version 2 for more details (a copy is included in the LICENSE file that
  * accompanied this code).
- *
+ * 
  * You should have received a copy of the GNU General Public License version
  * 2 along with this work; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
+ * 
  * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  */
-package org.graalvm.visualvm.graalvm;
+package org.graalvm.visualvm.graalvm.application.descriptor;
 
-import org.graalvm.visualvm.application.jvm.JvmFactory;
-import org.graalvm.visualvm.application.type.ApplicationTypeFactory;
-//import org.graalvm.visualvm.core.datasource.descriptor.DataSourceDescriptorFactory;
-//import org.graalvm.visualvm.graalvm.application.descriptor.NativeImageApplicationDescriptorProvider;
-import org.graalvm.visualvm.graalvm.application.type.GraalVMApplicationTypeFactory;
-import org.graalvm.visualvm.graalvm.svm.SVMJvmProvider;
-import org.openide.modules.ModuleInstall;
+import org.graalvm.visualvm.application.Application;
+import org.graalvm.visualvm.application.ApplicationDescriptor;
 
-public class Installer extends ModuleInstall {
-
-    @Override
-    public void restored() {
-        ApplicationTypeFactory.getDefault().registerProvider(new GraalVMApplicationTypeFactory());
-        
-        // NOTE: adds [native image] suffix to native-image processes
-//        DataSourceDescriptorFactory.getDefault().registerProvider(new NativeImageApplicationDescriptorProvider());
-        
-        JvmFactory.getDefault().registerProvider(new SVMJvmProvider());
+/**
+ *
+ * @author Jiri Sedlacek
+ */
+class NativeImageApplicationDescriptor extends ApplicationDescriptor {    
+    
+    protected NativeImageApplicationDescriptor(Application application) {
+        super(application);
     }
-
+    
+    public String getName() {
+        return super.getName() + " [native image]";
+    }
+    
 }
