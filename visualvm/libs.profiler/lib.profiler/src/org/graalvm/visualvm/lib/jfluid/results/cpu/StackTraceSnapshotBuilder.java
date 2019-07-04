ab9e8bb6af8b4a1b7e2cd17ffb728d37aa4d92cd
Tomas Hurka <tomas.hurka@gmail.com>
2019-07-04 10:37:52 +0200

    partial rollback of 'Friends for JFR viewer' (ec667d94465ac3520f6af9c6381e2cc8984e6179)

--- visualvm/libs.profiler/lib.profiler/src/org/graalvm/visualvm/lib/jfluid/results/cpu/StackTraceSnapshotBuilder.java
+++ visualvm/libs.profiler/lib.profiler/src/org/graalvm/visualvm/lib/jfluid/results/cpu/StackTraceSnapshotBuilder.java
@@ -185,18 +185,18 @@ public class StackTraceSnapshotBuilder {
         }
     }
     
-    public static class SampledThreadInfo {
+    static class SampledThreadInfo {
         private StackTraceElement[] stackTrace;
         private Thread.State state;
         private String threadName;
         private long threadId;
         private long threadCpuTime;
  
-        public SampledThreadInfo(String tn, long tid, Thread.State ts, StackTraceElement[] st, InstrumentationFilter filter) {
+        SampledThreadInfo(String tn, long tid, Thread.State ts, StackTraceElement[] st, InstrumentationFilter filter) {
             this (tn, tid,ts, st, -1, filter);
         }
 
-        public SampledThreadInfo(String tn, long tid, Thread.State ts, StackTraceElement[] st, long tct, InstrumentationFilter filter) {
+        SampledThreadInfo(String tn, long tid, Thread.State ts, StackTraceElement[] st, long tct, InstrumentationFilter filter) {
             threadName = tn;
             threadId = tid;
             state = ts;
@@ -224,7 +224,7 @@ public class StackTraceSnapshotBuilder {
             }
         }
         
-        public SampledThreadInfo(java.lang.management.ThreadInfo info, InstrumentationFilter filter) {
+        SampledThreadInfo(java.lang.management.ThreadInfo info, InstrumentationFilter filter) {
             this(info.getThreadName(), info.getThreadId(), info.getThreadState(), info.getStackTrace(), filter);
         }
         
@@ -329,7 +329,7 @@ public class StackTraceSnapshotBuilder {
         }
     }
     
-    final public void addStacktrace(SampledThreadInfo[] threads, long dumpTimeStamp) throws IllegalStateException {
+    final void addStacktrace(SampledThreadInfo[] threads, long dumpTimeStamp) throws IllegalStateException {
         long timediff = processDumpTimeStamp(dumpTimeStamp);
         
         if (timediff < 0) return;
