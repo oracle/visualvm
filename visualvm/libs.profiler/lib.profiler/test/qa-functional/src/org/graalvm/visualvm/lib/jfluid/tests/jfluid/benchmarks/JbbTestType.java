79e72ee8271a46e8de8a5be5094cb4f3a2da7419
Josh Soref <jsoref@users.noreply.github.com>
2020-09-10 23:57:19 -0400

    spelling: evaluate
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- visualvm/libs.profiler/lib.profiler/test/qa-functional/src/org/graalvm/visualvm/lib/jfluid/tests/jfluid/benchmarks/JbbTestType.java
+++ visualvm/libs.profiler/lib.profiler/test/qa-functional/src/org/graalvm/visualvm/lib/jfluid/tests/jfluid/benchmarks/JbbTestType.java
@@ -107,7 +107,7 @@ public abstract class JbbTestType extends CommonProfilerTestCase {
         return null;
     }
 
-    protected void evalueateResults(int[] without, int[] with, int maxSlowdown) {
+    protected void evaluateResults(int[] without, int[] with, int maxSlowdown) {
         double[] diffs = new double[with.length];
 
         for (int i = 0; i < diffs.length; i++) {
@@ -218,7 +218,7 @@ public abstract class JbbTestType extends CommonProfilerTestCase {
             log("finish ****************************** " + getStatus());
 
             int[] res2 = checkResults(workdir);
-            evalueateResults(res1, res2, maxSlowdown);
+            evaluateResults(res1, res2, maxSlowdown);
         } catch (Exception ex) {
             log(ex);
             assertTrue("Exception thrown: " + ex.getMessage(), false);
