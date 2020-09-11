0dd1296354b26dde39b7441158bbbc0f2c19cdaa
Josh Soref <jsoref@users.noreply.github.com>
2020-09-11 00:42:07 -0400

    spelling: superclasses
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- visualvm/libs.profiler/profiler.oql/test/unit/src/org/graalvm/visualvm/lib/profiler/oql/engine/api/impl/OQLEngineTest.java
+++ visualvm/libs.profiler/profiler.oql/test/unit/src/org/graalvm/visualvm/lib/profiler/oql/engine/api/impl/OQLEngineTest.java
@@ -311,7 +311,7 @@ public class OQLEngineTest {
     }
 
     @Test
-    public void testSuperlasses() throws Exception {
+    public void testSuperclasses() throws Exception {
         System.out.println("superclasses");
         final int[] counter = new int[1];
 
