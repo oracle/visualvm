730dd5bcfe94c801d8e5ee4128f1c8f4fdddea10
Josh Soref <jsoref@users.noreply.github.com>
2020-09-11 00:36:08 -0400

    spelling: parallelize
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- visualvm/libs.profiler/profiler.heapwalker/src/org/graalvm/visualvm/lib/profiler/heapwalk/memorylint/IteratingRule.java
+++ visualvm/libs.profiler/profiler.heapwalker/src/org/graalvm/visualvm/lib/profiler/heapwalk/memorylint/IteratingRule.java
@@ -59,7 +59,7 @@ import javax.swing.JComponent;
  * An iterating rule is a rule that iterates through all instances of single
  * class and does some analysis over each instance. When such a Rule is based
  * on this helper class, the infrastructure can independently monitor
- * the progress and also paralelize the task among available CPUs.
+ * the progress and also parallelize the task among available CPUs.
  *
  * Rules can override {@link #prepareRule(MemoryLint)} and {@link #summary()}
  * for preparation and finalization work, and must implement
