57e8272936335cb80e79ad78be95c7b2a0e9ed73
Jiri Sedlacek <jiri.sedlacek@oracle.com>
2020-12-14 14:22:31 +0100

    Implemented GH-229 - Settings migration from another VisualVM userdir

---
+++ visualvm/startup/src/org/graalvm/visualvm/modules/startup/ImportPanel.java
@@ -0,0 +1,413 @@
+/*
+ * Copyright (c) 2020, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package org.graalvm.visualvm.modules.startup;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ItemEvent;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.io.File;
+import java.util.concurrent.ExecutionException;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFileChooser;
+import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+import javax.swing.JRadioButton;
+import javax.swing.JTextPane;
+import javax.swing.SwingWorker;
+import javax.swing.UIManager;
+import javax.swing.filechooser.FileFilter;
+import org.openide.util.NbBundle;
+
+/**
+ *
+ * @author Jiri Sedlacek
+ */
+abstract class ImportPanel extends JPanel {
+    
+    private static final String KEY_FOLDER = "settings_folder";                 // NOI18N
+    
+    
+    private final JProgressBar progress;
+    private final JTextPane text2;
+    private final JButton importB;
+    private final JButton skipB;
+    private final JRadioButton latest;
+    private final JRadioButton recent;
+    private final JRadioButton custom;
+    
+    private File selected;
+    private File userdirsRoot;
+    
+    
+    ImportPanel(final File latestRelease, final File recentlyUsed, File userdirsRoot) {
+        super(new GridBagLayout());
+        
+        GridBagConstraints c;
+        
+        c = new GridBagConstraints();
+        c.gridx = 0;
+        c.gridy = 0;
+        c.insets = new Insets(25, 15, 10, 15);
+        c.anchor = GridBagConstraints.NORTHWEST;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridwidth = GridBagConstraints.REMAINDER;
+        JTextPane text1 = new JTextPane();
+        text1.setContentType("text/html");                                      // NOI18N
+        text1.setFont(UIManager.getFont("Label.font"));                         // NOI18N
+        text1.setOpaque(false);
+        text1.setEditable(false);
+        text1.setFocusable(false);
+        text1.setRequestFocusEnabled(false);
+        setHtmlText(text1, NbBundle.getMessage(ImportPanel.class, "ImportPanel_Msg1")); // NOI18N
+        add(text1, c);
+        
+        c = new GridBagConstraints();
+        c.gridx = 0;
+        c.gridy = 1;
+        c.insets = new Insets(0, 25, 0, 15);
+        c.anchor = GridBagConstraints.WEST;
+        c.fill = GridBagConstraints.NONE;
+        c.gridwidth = GridBagConstraints.REMAINDER;
+        latest = new JRadioButton() {
+            protected void fireItemStateChanged(ItemEvent event) {
+                super.fireItemStateChanged(event);
+                if (event.getStateChange() == ItemEvent.SELECTED) {
+                    selected = latestRelease;
+                }
+            }
+        };
+        latest.setText(getHtmlText(latest, "<nobr>" + latestRelease.getName() + " <span style=\"color:" + getColorText(UIManager.getColor("Label.disabledForeground")) + ";\">" + // NOI18N
+                                           NbBundle.getMessage(ImportPanel.class, "ImportPanel_OptionLatestRelease") + "</span>" + "</nobr>")); // NOI18N
+        latest.putClientProperty(KEY_FOLDER, latestRelease);
+        latest.setSelected(true);
+        latest.setToolTipText(latestRelease.getAbsolutePath());
+        add(latest, c);
+        
+        if (recentlyUsed != null && !recentlyUsed.equals(latestRelease)) {
+            c = new GridBagConstraints();
+            c.gridx = 0;
+            c.gridy = 2;
+            c.insets = new Insets(0, 25, 0, 15);
+            c.anchor = GridBagConstraints.WEST;
+            c.fill = GridBagConstraints.NONE;
+            c.gridwidth = GridBagConstraints.REMAINDER;
+            recent = new JRadioButton() {
+                protected void fireItemStateChanged(ItemEvent event) {
+                    super.fireItemStateChanged(event);
+                    if (event.getStateChange() == ItemEvent.SELECTED) {
+                        selected = recentlyUsed;
+                    }
+                }
+            };
+            recent.setText(getHtmlText(recent, "<nobr>" + recentlyUsed.getName() + " <span style=\"color:" + getColorText(UIManager.getColor("Label.disabledForeground")) + ";\">" + // NOI18N
+                                           NbBundle.getMessage(ImportPanel.class, "ImportPanel_OptionRecentlyUsed") + "</span>" + "</nobr>")); // NOI18N
+            recent.setToolTipText(recentlyUsed.getAbsolutePath());
+            recent.putClientProperty(KEY_FOLDER, recentlyUsed);
+            add(recent, c);
+        } else {
+            recent = null;
+        }
+        
+        c = new GridBagConstraints();
+        c.gridx = 0;
+        c.gridy = 3;
+        c.insets = new Insets(0, 25, 0, 15);
+        c.anchor = GridBagConstraints.WEST;
+        c.fill = GridBagConstraints.NONE;
+        custom = new JRadioButton() {
+            protected void fireItemStateChanged(ItemEvent event) {
+                super.fireItemStateChanged(event);
+                if (event.getStateChange() == ItemEvent.SELECTED) {
+                    selected = null;
+                }
+            }
+        };
+        custom.setText(getHtmlText(custom, "<nobr>" + NbBundle.getMessage(ImportPanel.class, "ImportPanel_OptionSelectCustom") + "</nobr>")); // NOI18N
+        custom.setToolTipText(NbBundle.getMessage(ImportPanel.class, "ImportPanel_OptionSelectCustomTooltip")); // NOI18N
+        custom.putClientProperty(KEY_FOLDER, null);
+        add(custom, c);
+        
+        c = new GridBagConstraints();
+        c.gridx = 0;
+        c.gridy = 4;
+        c.insets = new Insets(30, 15, 10, 15);
+        c.anchor = GridBagConstraints.WEST;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridwidth = GridBagConstraints.REMAINDER;
+        progress = new JProgressBar();
+        progress.setString(NbBundle.getMessage(ImportPanel.class, "ImportPanel_ProgressText")); // NOI18N
+        progress.setStringPainted(true);
+        progress.setVisible(false);
+        add(progress, c);
+        
+        c = new GridBagConstraints();
+        c.gridx = 0;
+        c.gridy = 5;
+        c.insets = new Insets(30, 15, 10, 15);
+        c.anchor = GridBagConstraints.NORTHWEST;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridwidth = GridBagConstraints.REMAINDER;
+        text2 = new JTextPane();
+        text2.setContentType("text/html");                                      // NOI18N
+        text2.setFont(UIManager.getFont("Label.font"));                         // NOI18N
+        text2.setOpaque(false);
+        text2.setEditable(false);
+        text2.setFocusable(false);
+        text2.setRequestFocusEnabled(false);
+        setHtmlText(text2, NbBundle.getMessage(ImportPanel.class, "ImportPanel_Msg2")); // NOI18N
+        add(text2, c);
+        
+        c = new GridBagConstraints();
+        c.gridx = 0;
+        c.gridy = 6;
+        c.weightx = 1;
+        c.weighty = 1;
+        c.insets = new Insets(0, 0, 0, 0);
+        c.anchor = GridBagConstraints.NORTHWEST;
+        c.fill = GridBagConstraints.BOTH;
+        c.gridwidth = GridBagConstraints.REMAINDER;
+        JPanel f = new JPanel(null);
+        f.setOpaque(false);
+        add(f, c);
+        
+        c = new GridBagConstraints();
+        c.gridx = 0;
+        c.gridy = 7;
+        c.weightx = 1;
+        c.insets = new Insets(25, 15, 10, 5);
+        c.anchor = GridBagConstraints.EAST;
+        c.fill = GridBagConstraints.NONE;
+        importB = new JButton() {
+            protected void fireActionPerformed(ActionEvent e) {
+                super.fireActionPerformed(e);
+                importImpl();
+            }
+        };
+        importB.setText(NbBundle.getMessage(ImportPanel.class, "ImportPanel_ImportButton")); // NOI18N
+        importB.setDefaultCapable(true);
+        add(importB, c);
+        
+        c = new GridBagConstraints();
+        c.gridx = 1;
+        c.gridy = 7;
+        c.insets = new Insets(25, 0, 10, 10);
+        c.anchor = GridBagConstraints.EAST;
+        c.fill = GridBagConstraints.NONE;
+        skipB = new JButton() {
+            protected void fireActionPerformed(ActionEvent e) {
+                super.fireActionPerformed(e);
+                skipImpl();
+            }
+        };
+        skipB.setText(NbBundle.getMessage(ImportPanel.class, "ImportPanel_SkipButton")); // NOI18N
+        skipB.setDefaultCapable(false);
+        add(skipB, c);
+        
+        ButtonGroup userdirs = new ButtonGroup();
+        userdirs.add(latest);
+        if (recent != null) userdirs.add(recent);
+        userdirs.add(custom);
+        
+        makeSameSize(importB, skipB);
+        
+        selected = latestRelease;
+        this.userdirsRoot = userdirsRoot;
+    }
+    
+    
+    abstract void contentsChanged();
+    
+    abstract void beforeImport();
+    
+    abstract void doImport(File source) throws Exception;
+    
+    abstract void afterImport();
+    
+    abstract void close();
+    
+    
+    JButton getDefaultButton() {
+        return importB;
+    }
+    
+    
+    private void importImpl() {
+        final File source;
+        if (selected == null) {
+            source = customImpl();
+            if (source == null) return;
+        } else {
+            source = selected;
+        }
+        
+        beforeImport();
+        
+        new SwingWorker<String, Void>() {
+            {
+                if (latest.getClientProperty(KEY_FOLDER) != selected) latest.setEnabled(false);
+                if (recent != null && recent.getClientProperty(KEY_FOLDER) != selected) recent.setEnabled(false);
+                if (custom.getClientProperty(KEY_FOLDER) != selected) custom.setEnabled(false);
+                importB.setEnabled(false);
+                skipB.setEnabled(false);
+                text2.setVisible(false);
+                progress.setIndeterminate(true);
+                progress.setVisible(true);
+            }
+            @Override
+            protected String doInBackground() throws Exception {
+                try {
+                    doImport(source);
+                    return null;
+                } catch (Exception e) {
+                    return e.getLocalizedMessage();
+                }
+            }
+            @Override
+            public void done() {
+                afterImport();
+                
+                String result;
+                try {
+                    result = get();
+                } catch (InterruptedException | ExecutionException ex) {
+                    result = ex.getLocalizedMessage();
+                }
+                
+                progress.setIndeterminate(false);
+                
+                if (result != null) {
+                    progress.setVisible(false);
+                    setHint("<nobr><b>" + NbBundle.getMessage(ImportPanel.class, "ImportPanel_ImportFailed") + "</b><br>" + result + "</nobr>"); // NOI18N
+                    text2.setVisible(true);
+                    importB.setEnabled(true);
+                    skipB.setEnabled(true);
+                    latest.setEnabled(true);
+                    if (recent != null) recent.setEnabled(true);
+                    custom.setEnabled(true);
+                } else {
+                    close();
+                }
+            }
+        }.execute();
+    }
+    
+    private void skipImpl() {
+        close();
+    }
+    
+    private File customImpl() {
+        JFileChooser ch = new JFileChooser(userdirsRoot) {
+            public void approveSelection() {
+                if (NbBundle.getMessage(ImportPanel.class, "ImportPanel_ImportButton").equals(getApproveButtonText())) { // NOI18N
+                    super.approveSelection();
+                } else {
+                    File f = getSelectedFile();
+                    if (f == null) f = getCurrentDirectory();
+                    setCurrentDirectory(f);
+                }
+            }
+        };
+        ch.setDialogType(JFileChooser.CUSTOM_DIALOG);
+        ch.setDialogTitle(NbBundle.getMessage(ImportPanel.class, "ImportPanel_CustomDirCaption")); // NOI18N
+        ch.setApproveButtonText(NbBundle.getMessage(ImportPanel.class, "ImportPanel_OpenButton")); // NOI18N
+        ch.setApproveButtonToolTipText(NbBundle.getMessage(ImportPanel.class, "ImportPanel_OpenButtonTooltip")); // NOI18N
+        ch.setMultiSelectionEnabled(false);
+        ch.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        ch.setAcceptAllFileFilterUsed(false);
+        ch.addChoosableFileFilter(new FileFilter() {
+            @Override public boolean accept(File f) { return f.isDirectory(); }
+            @Override public String getDescription() { return NbBundle.getMessage(ImportPanel.class, "ImportPanel_SettingsFilter"); } // NOI18N
+        });
+        ch.setSelectedFile(null);
+        ch.addPropertyChangeListener(new PropertyChangeListener() {
+            public void propertyChange(final PropertyChangeEvent evt) {
+                if (JFileChooser.SELECTED_FILE_CHANGED_PROPERTY.equals(evt.getPropertyName()) ||
+                    JFileChooser.DIRECTORY_CHANGED_PROPERTY.equals(evt.getPropertyName())) {
+                    File f = ch.getSelectedFile();
+                    if (f == null) f = ch.getCurrentDirectory();
+                    if (f != null && f.isDirectory() && new File(f, "config").isDirectory()) { // NOI18N
+                        ch.setApproveButtonText(NbBundle.getMessage(ImportPanel.class, "ImportPanel_ImportButton")); // NOI18N
+                        ch.setApproveButtonToolTipText(NbBundle.getMessage(ImportPanel.class, "ImportPanel_ImportButtonTooltip")); // NOI18N
+                    } else {
+                        ch.setApproveButtonText(NbBundle.getMessage(ImportPanel.class, "ImportPanel_OpenButton")); // NOI18N
+                        ch.setApproveButtonToolTipText(NbBundle.getMessage(ImportPanel.class, "ImportPanel_OpenButtonTooltip")); // NOI18N
+                    }
+                }
+            }
+        });
+        
+        if (ch.showDialog(latest, null) == JFileChooser.APPROVE_OPTION) {
+            File userdir = ch.getSelectedFile();
+            userdirsRoot = userdir == null ? null : userdir.getParentFile();
+            return userdir;
+        } else {
+            return null;
+        }
+    }
+    
+    
+    private void setHint(String text) {
+        setHtmlText(text2, text);
+        contentsChanged();
+    }
+    
+    
+    private static void makeSameSize(JButton button1, JButton button2) {
+        Dimension dim1 = button1.getPreferredSize();
+        Dimension dim2 = button2.getPreferredSize();
+        int maxWidth = Math.max(dim1.width, dim2.width);
+        int maxHeight = Math.max(dim1.height, dim2.height);
+        button1.setPreferredSize(new Dimension(maxWidth, maxHeight));
+        button2.setPreferredSize(new Dimension(maxWidth, maxHeight));
+    }
+    
+    
+    private static void setHtmlText(JTextPane pane, String text) {
+        pane.setDocument(pane.getEditorKit().createDefaultDocument()); // Workaround for http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=5042872
+        pane.setText(getHtmlText(pane, text));
+        pane.setCaretPosition(0);
+    }
+    
+    private static String getHtmlText(JComponent component, String text) {
+        Font font = component.getFont();
+        String rgb = getColorText(component.getForeground());
+        return "<html><body text=\"" + rgb + "\" style=\"font-size: " + font.getSize() + // NOI18N
+               "pt; font-family: " + font.getName() + ";\">" + text + "</body></html>"; // NOI18N
+    }
+    
+    private static String getColorText(Color color) {
+        return "rgb(" + color.getRed() + "," + color.getGreen() + "," + color.getBlue() + ")"; // NOI18N
+    }
+    
+}
---
+++ visualvm/startup/src/org/graalvm/visualvm/modules/startup/ImportSettings.java
@@ -0,0 +1,256 @@
+/*
+ * Copyright (c) 2020, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package org.graalvm.visualvm.modules.startup;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.beans.PropertyVetoException;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Logger;
+import javax.swing.JComponent;
+import javax.swing.JDialog;
+import javax.swing.KeyStroke;
+import javax.swing.SwingUtilities;
+import org.graalvm.visualvm.modules.startup.dialogs.StartupDialog;
+import org.openide.modules.InstalledFileLocator;
+import org.openide.util.NbBundle;
+
+/**
+ * Imports VisualVM settings from another compatible VisualVM userdir.
+ * 
+ * To test right from NetBeans:
+ * 
+ *  1a/ Predefined import:
+ *     - add -J-Dnetbeans.import_userdir=... to visualvm/nbproject/project.properties
+ *
+ *  1b/ Interactive import:
+ *     - add -J-Dnetbeans.importclass=org.graalvm.visualvm.modules.startup.ImportSettings to visualvm/nbproject/project.properties
+ *     - add -J-Dnetbeans.default_userdir_root.dev=... to visualvm/nbproject/project.properties
+ * 
+ *  2/ Uncomment lines marked as *DEV*
+ * 
+ *  3/ Make sure the development userdir visualvm/build/testuserdir does not exist
+ *
+ *  4/ Invoke Run on the VisualVM-Startup module
+ *
+ * 
+ * @author Jiri Sedlacek
+ */
+public class ImportSettings {
+    
+    // List of userdirs supported by the settings importer
+    // Most recent release userdir is the last one
+    private static final String[] SUPPORTED_USERDIRS = new String[] {
+        "2.0",                                                                  // NOI18N
+        "2.0.1",                                                                // NOI18N
+        "2.0.2",                                                                // NOI18N
+        "2.0.3",                                                                // NOI18N
+        "2.0.4",                                                                // NOI18N
+        "2.0.5"                                                                 // NOI18N
+    };
+    
+    
+    private final static Logger LOGGER = Logger.getLogger(ImportSettings.class.getName());
+    
+    
+    public static void main(String[] args) throws Exception {
+        String importUserdirS = System.getProperty("netbeans.import_userdir");  // NOI18N
+        if (importUserdirS != null && !importUserdirS.isEmpty()) {
+            // immediate import based on the provided system property
+            File importUserdir = new File(importUserdirS);
+            if (importUserdir.isDirectory()) {
+                copyToUserdir(importUserdir);
+            } else {
+                LOGGER.info("Skipping import from netbeans.import_userdir, wrong userdir provided " + importUserdir); // NOI18N
+            }
+        } else {
+            // interactive selection & import of the userdir
+            String userdirsRootS = System.getProperty("netbeans.default_userdir_root"); // NOI18
+///*DEV*/     if (userdirsRootS == null || userdirsRootS.isEmpty()) userdirsRootS = System.getProperty("netbeans.default_userdir_root.dev"); // NOI18
+
+            File userdirsRoot = userdirsRootS == null ? null : new File(userdirsRootS);
+            if (userdirsRoot == null || !userdirsRoot.isDirectory()) return;
+
+            List<File> userdirs = availableUserdirs(userdirsRoot);
+            if (userdirs.isEmpty()) return;
+
+            File latestRelease = latestReleaseUserdir(userdirs);
+            File recentlyUsed = lastRecentlyUsedUserdir(userdirs);
+
+
+            Utils.setSystemLaF();
+
+            final JDialog d = StartupDialog.create(NbBundle.getMessage(
+                    ImportSettings.class, "ImportSettings_Caption"), null, -1); // NOI18N
+
+            ImportPanel p = new ImportPanel(latestRelease, recentlyUsed, userdirsRoot) {
+                @Override
+                void contentsChanged() {
+                    SwingUtilities.invokeLater(new Runnable() {
+                        public void run() { d.pack(); }
+                    });
+                }
+
+                @Override
+                void beforeImport() {
+                    d.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
+                }
+
+                @Override
+                void doImport(File source) throws Exception {
+                    copyToUserdir(source);
+                }
+
+                @Override
+                void afterImport() {
+                    d.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+                }
+
+                @Override
+                void close() {
+                    d.setVisible(false);
+                    d.dispose();
+                }
+            };
+            d.getContentPane().add(p, BorderLayout.CENTER);
+
+            d.getRootPane().setDefaultButton(p.getDefaultButton());
+            d.getRootPane().registerKeyboardAction(new ActionListener() {
+                    @Override public void actionPerformed(ActionEvent e) {
+                        if (d.getDefaultCloseOperation() == JDialog.DISPOSE_ON_CLOSE) {
+                            d.setVisible(false);
+                            d.dispose();
+                        }
+                    }
+                },
+                KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0),
+                JComponent.WHEN_IN_FOCUSED_WINDOW);
+
+    //        d.setResizable(true);
+            d.pack();
+            d.setLocationRelativeTo(null);
+
+            d.setVisible(true);
+        }
+    }
+    
+    
+    private static List<File> availableUserdirs(File userdirsRoot) {
+        List<File> userdirs = new ArrayList();
+        
+        for (String supported : SUPPORTED_USERDIRS) {
+            File userdir = availableUserdir(userdirsRoot, supported);
+            if (userdir != null) userdirs.add(userdir);
+        }
+        
+        return userdirs;
+    }
+    
+    private static File availableUserdir(File userdirsFolder, String userdir) {
+        File file = new File(userdirsFolder, userdir);
+        
+        if (!file.isDirectory() || !new File(file, "config").isDirectory()) return null; // NOI18N
+        
+        return file;
+    }
+    
+    private static File latestReleaseUserdir(List<File> userdirs) {
+        return userdirs.isEmpty() ? null : userdirs.get(userdirs.size() - 1);
+    }
+    
+    private static File lastRecentlyUsedUserdir(List<File> userdirs) {
+        File lastRecent = null;
+        long lastModified = -1;
+                
+        for (File userdir : userdirs) {
+            long modified = userdir.lastModified();
+            if (modified > lastModified) {
+                lastModified = modified;
+                lastRecent = userdir;
+            }
+        }
+        
+        return lastRecent;
+    }
+    
+    
+    /* Copy files from source folder to current userdir according to include/exclude
+     * patterns in etc/netbeans.import file. */
+    private static void copyToUserdir(File source) throws IOException, PropertyVetoException {
+        File userdir = new File(System.getProperty("netbeans.user", ""));       // NOI18N
+        File netBeansDir = InstalledFileLocator.getDefault().locate("modules", null, false).getParentFile().getParentFile(); // NOI18N
+        
+        File importFile = new File(netBeansDir, "etc/visualvm.import");         // NOI18N
+///*DEV*/ if (!importFile.isFile() && "testuserdir".equals(userdir.getName())) {  // NOI18N
+///*DEV*/     File parent = netBeansDir.getParentFile();
+///*DEV*/     if (parent != null && parent.isDirectory()) {
+///*DEV*/         importFile = new File(parent, "launcher/visualvm.import");      // NOI18N
+///*DEV*/     }
+///*DEV*/ }
+        
+        LOGGER.fine("Import file: " + importFile);                              // NOI18N
+        LOGGER.info("Importing from " + source + " to " + userdir);             // NOI18N
+        
+        File[] files1 = userdir.listFiles();
+        try {
+            CopyFiles.copyDeep(source, userdir, importFile);
+        } catch (Exception e) {
+            try {
+                cleanupUserdir(userdir, files1);
+            } catch (Exception ee) {
+                LOGGER.info("Failed to cleanup after import failure " + ee);    // NOI18N
+            }
+            throw e;
+        }
+        
+    }
+    
+    
+    private static void cleanupUserdir(File userdir, File[] initialFiles) {
+        // Delete newly created directories
+        File[] files = userdir.listFiles();
+        if (initialFiles.length != files.length) {
+            List<File> imported = new ArrayList(Arrays.asList(files));
+            imported.removeAll(Arrays.asList(initialFiles));
+            for (File f : imported) delete(f);
+        }
+        
+        // Cleanup config directory (created but empty before the import)
+        File config = new File(userdir, "config");                              // NOI18N
+        for (File f : config.listFiles()) delete(f);
+    }
+    
+    private static void delete(File file) {
+        if (file.isDirectory()) for (File f : file.listFiles()) delete(f);
+        file.delete();
+    }
+    
+}
