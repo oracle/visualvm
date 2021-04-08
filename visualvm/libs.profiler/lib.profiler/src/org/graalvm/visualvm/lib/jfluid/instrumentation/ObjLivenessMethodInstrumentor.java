bbbdd943012aecdb30908a5926d94eebf5e38fe5
Tomas Hurka <tomas.hurka@gmail.com>
2021-04-08 13:50:58 +0200

    unused method removed

--- visualvm/libs.profiler/lib.profiler/src/org/graalvm/visualvm/lib/jfluid/instrumentation/ObjLivenessMethodInstrumentor.java
+++ visualvm/libs.profiler/lib.profiler/src/org/graalvm/visualvm/lib/jfluid/instrumentation/ObjLivenessMethodInstrumentor.java
@@ -281,10 +281,6 @@ public class ObjLivenessMethodInstrumentor extends MemoryProfMethodInstrumentor 
         }
     }
     
-    private boolean trackAllAllocations() {
-        return instrFilter.getType() == InstrumentationFilter.TYPE_NONE && allUnprofiledClassStatusArray == null;
-    }
-    
     static boolean isObjectConstructor(ClassInfo clazz, int methodIdx) {
         if (OBJECT_SLASHED_CLASS_NAME.equals(clazz.getName())
                 && clazz.getLoaderId() <= 0
