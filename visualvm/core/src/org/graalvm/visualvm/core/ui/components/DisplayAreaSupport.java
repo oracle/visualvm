fb85a659cfd71369b0c5a52262b976bf676b17fa
Tomas Hurka <tomas.hurka@gmail.com>
2021-06-09 16:42:28 +0200

    year in copyright notice updated

--- visualvm/core/src/org/graalvm/visualvm/core/ui/components/DisplayAreaSupport.java
+++ visualvm/core/src/org/graalvm/visualvm/core/ui/components/DisplayAreaSupport.java
@@ -3,7 +3,7 @@
  *
  * Created on Dec 11, 2007, 2:20:48 PM
  *
- * Copyright (c) 2007, 2011, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2007, 2018, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
