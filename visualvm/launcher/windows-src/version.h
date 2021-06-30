ca67851796fe61f9240315e76cd077038f8b802e
Tomas Hurka <tomas.hurka@gmail.com>
2021-06-30 14:22:08 +0200

    VisualVM 2.1 branding

--- visualvm/launcher/windows-src/version.h
+++ visualvm/launcher/windows-src/version.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007, 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2007, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -25,11 +25,11 @@
 
 #define COMPANY "Oracle Corporation"
 #define COMPONENT "VisualVM"
-#define VER "0.0.0.0"
-#define FVER 0,0,0,0
-#define BUILD_ID "0"
+#define VER "2.1.0.0"
+#define FVER 2,1,0,0
+#define BUILD_ID "30062021"
 #define INTERNAL_NAME "visualvm"
-#define COPYRIGHT "\xA9 2007, 2014, Oracle and/or its affiliates. All rights reserved."
+#define COPYRIGHT "\xA9 2007, 2021, Oracle and/or its affiliates. All rights reserved."
 #define FNAME "visualvm.exe"
-#define NAME "VisualVM Dev. build"
+#define NAME "VisualVM"
 
