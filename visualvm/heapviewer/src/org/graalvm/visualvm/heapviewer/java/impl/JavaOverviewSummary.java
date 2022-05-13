0dba3eafde9a3c9b95dcdf599991bbd77328dec2
Tomas Hurka <tomas.hurka@gmail.com>
2022-05-13 09:41:25 +0200

    unused import removed

--- visualvm/heapviewer/src/org/graalvm/visualvm/heapviewer/java/impl/JavaOverviewSummary.java
+++ visualvm/heapviewer/src/org/graalvm/visualvm/heapviewer/java/impl/JavaOverviewSummary.java
@@ -49,7 +49,6 @@ import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
