53e1c5580792637c5a964bc5193e4fc2e9f00095
Tomas Hurka <tomas.hurka@gmail.com>
2022-06-16 12:38:47 +0200

    VisualVM 2.1.4 branding

--- visualvm/startup/src/org/graalvm/visualvm/modules/startup/ImportSettings.java
+++ visualvm/startup/src/org/graalvm/visualvm/modules/startup/ImportSettings.java
@@ -83,7 +83,8 @@ public class ImportSettings {
         "2.1",                                                                  // NOI18N
         "2.1.1",                                                                // NOI18N
         "2.1.2",                                                                // NOI18N
-        "2.1.3"                                                                 // NOI18N
+        "2.1.3",                                                                // NOI18N
+        "2.1.4"                                                                 // NOI18N
     };
     
     
