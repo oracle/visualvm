90536c450089374e52fc50ae644f427a5c0bc027
Tomas Hurka <tomas.hurka@gmail.com>
2024-07-08 15:49:53 +0200

    VisualVM 2.1.9 branding

--- visualvm/appui/src/org/graalvm/visualvm/modules/appui/AboutAction.java
+++ visualvm/appui/src/org/graalvm/visualvm/modules/appui/AboutAction.java
@@ -51,7 +51,7 @@ import org.openide.windows.WindowManager;
 public final class AboutAction extends AbstractAction {
     private final static Logger LOGGER = Logger.getLogger(AboutAction.class.getName());
     
-    private String versionString = "Dev"; // Use "Dev" for development builds   // NOI18N
+    private String versionString = "2.1.9"; // Use "Dev" for development builds   // NOI18N
 
 
     public AboutAction() {
