21bb2d121f99795cc78634d2d99eb6428e1e90a5
Tomas Hurka <tomas.hurka@gmail.com>
2026-06-15 11:42:22 +0200

    VisualVM 2.2.2 branding

--- visualvm/launcher/windows-src/nbproject/Makefile-variables.mk
+++ visualvm/launcher/windows-src/nbproject/Makefile-variables.mk
@@ -14,6 +14,14 @@ CND_ARTIFACT_PATH_visualvm.exe=visualvm.exe
 CND_PACKAGE_DIR_visualvm.exe=dist/visualvm.exe/Cygwin-Windows/package
 CND_PACKAGE_NAME_visualvm.exe=windows-src.tar
 CND_PACKAGE_PATH_visualvm.exe=dist/visualvm.exe/Cygwin-Windows/package/windows-src.tar
+# visualvm64.exe configuration
+CND_PLATFORM_visualvm64.exe=Cygwin64-Windows
+CND_ARTIFACT_DIR_visualvm64.exe=
+CND_ARTIFACT_NAME_visualvm64.exe=visualvm64.exe
+CND_ARTIFACT_PATH_visualvm64.exe=visualvm64.exe
+CND_PACKAGE_DIR_visualvm64.exe=dist/visualvm64.exe/Cygwin64-Windows/package
+CND_PACKAGE_NAME_visualvm64.exe=windows-src.tar
+CND_PACKAGE_PATH_visualvm64.exe=dist/visualvm64.exe/Cygwin64-Windows/package/windows-src.tar
 #
 # include compiler specific variables
 #
